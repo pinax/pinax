af9d4bf7716140e031b2949c7b4d43ef92d6cfce
James Tauber <jtauber@jtauber.com>
2008-05-16 22:00:59 +0000

    fixed friends display when not logged in
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@37 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/friends_app/views.py
+++ projects/pinax/friends_app/views.py
@@ -3,17 +3,21 @@ from django.template import RequestContext
 from friends.models import Friendship, FriendshipInvitation
 
 def friends(request):
-    if request.method == "POST":
-        if request.POST["action"] == "accept":
-            invitation_id = request.POST["invitation"]
-            try:
-                invitation = FriendshipInvitation.objects.get(id=invitation_id)
-                if invitation.to_user == request.user:
-                    invitation.accept()
-                    request.user.message_set.create(message="Accepted friendship request from %s" % invitation.from_user)
-            except FriendshipInvitation.DoesNotExist:
-                pass
+    if request.user.is_authenticated():
+        if request.method == "POST":
+            if request.POST["action"] == "accept":
+                invitation_id = request.POST["invitation"]
+                try:
+                    invitation = FriendshipInvitation.objects.get(id=invitation_id)
+                    if invitation.to_user == request.user:
+                        invitation.accept()
+                        request.user.message_set.create(message="Accepted friendship request from %s" % invitation.from_user)
+                except FriendshipInvitation.DoesNotExist:
+                    pass
+        friends = Friendship.objects.friends_for_user(request.user)
+    else:
+        friends = None
     return render_to_response("friends_app/friends.html", {
-        "friends": Friendship.objects.friends_for_user(request.user),
+        "friends": friends,
     }, context_instance=RequestContext(request))
     
