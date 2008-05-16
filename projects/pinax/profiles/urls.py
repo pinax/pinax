91095d7f17ac69cec6162e0764b5b53180373586
James Tauber <jtauber@jtauber.com>
2008-05-16 17:28:04 +0000

    added an overall profiles page and a friend invite on each profile page
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@30 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/profiles/urls.py
+++ projects/pinax/profiles/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+    (r'^$', 'profiles.views.profiles'),
     (r'^(\w+)/$', 'profiles.views.profile'),
 )
--- projects/pinax/profiles/views.py
+++ projects/pinax/profiles/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.models import User
 
+from friends.forms import InviteFriendForm
+from friends.models import FriendshipInvitation
+
+def profiles(request):
+    return render_to_response("profiles/profiles.html", {
+        "users": User.objects.all(),
+    }, context_instance=RequestContext(request))
+
 def profile(request, username):
     other_user = get_object_or_404(User, username=username)
+    if request.method == "POST":
+        invite_form = InviteFriendForm(request.user, request.POST)
+        if invite_form.is_valid():
+            invite_form.save()
+    else:
+        invite_form = InviteFriendForm(request.user, {
+            'to_user': username,
+        })
+    previous_invitations = FriendshipInvitation.objects.filter(to_user=other_user, from_user=request.user)
     return render_to_response("profiles/profile.html", {
         "other_user": other_user,
+        "invite_form": invite_form,
+        "previous_invitations": previous_invitations,
     }, context_instance=RequestContext(request))
