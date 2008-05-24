a404f384b1d0ced01de9c0f83441bf134a15b676
James Tauber <jtauber@jtauber.com>
2008-05-24 09:28:00 +0000

    beginnings of move to django-profiles
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@73 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/profiles_app/models.py
+++ projects/pinax/profiles_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    
+    user = models.ForeignKey(User, unique=True)
+    
+    class Admin:
+        pass
--- projects/pinax/profiles_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'^$', 'profiles_app.views.profiles'),
-    (r'^(\w+)/$', 'profiles_app.views.profile'),
-)
