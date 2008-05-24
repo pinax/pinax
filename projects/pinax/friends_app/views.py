090f5901a9e15a515e51d4544a82fa5d981f3f90
James Tauber <jtauber@jtauber.com>
2008-05-24 20:28:15 +0000

    first part of join invitation -- you can send join invitations but nothing accepts them yet
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@99 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/friends_app/views.py
+++ projects/pinax/friends_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from friends.models import Friendship, FriendshipInvitation
+from friends.forms import JoinRequestForm
 
 def friends(request):
     if request.user.is_authenticated():
@@ -14,6 +15,16 @@ def friends(request):
                         request.user.message_set.create(message="Accepted friendship request from %s" % invitation.from_user)
                 except FriendshipInvitation.DoesNotExist:
                     pass
+            elif request.POST["action"] == "invite": # invite to join
+                join_request_form = JoinRequestForm(request.POST)
+                if join_request_form.is_valid():
+                    join_request_form.save(request.user)
+                    join_request_form = JoinRequestForm() # @@@
+        else:
+            join_request_form = JoinRequestForm()
+    else:
+        join_request_form = None
     return render_to_response("friends_app/invitations.html", {
+        "join_request_form": join_request_form,
     }, context_instance=RequestContext(request))
     
