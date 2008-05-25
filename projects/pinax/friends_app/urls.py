e12a2c96cfb90a5381566448be3b1b3a6e40536e
Brian Rosner <brosner@gmail.com>
2008-05-25 19:03:43 +0000

    Removed the hardcoded URL to accept_join for join invite e-mails.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@129 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/friends_app/urls.py
+++ projects/pinax/friends_app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^$', 'friends_app.views.friends'),
-    (r'^accept/(\w+)/$', 'friends_app.views.accept_join'),
+    url(r'^$', 'friends_app.views.friends'),
+    url(r'^accept/(\w+)/$', 'friends_app.views.accept_join', name='friends_accept_join'),
 )
