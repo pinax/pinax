23792a43374460b9ee5d7ca04014f7931febe3ba
James Tauber <jtauber@jtauber.com>
2008-05-26 13:54:04 +0000

    removed authopenid too
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@164 45601e1e-1555-4799-bd40-45c8c71eef50

--- apps/authopenid/middleware.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-class OpenIDMiddleware(object):
-    """
-    Populate request.openid. This comes either from cookie or from
-    session, depending on the presence of OPENID_USE_SESSIONS.
-    """
-    def process_request(self, request):
-        request.openid = request.session.get('openid', None)
