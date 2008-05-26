f70063b877c076039f9f88a1a1007100cdb59a75
James Tauber <jtauber@jtauber.com>
2008-05-26 13:28:03 +0000

    html escape gravatar img src
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@162 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/gravatar/templatetags/gravatar.py
+++ projects/pinax/gravatar/templatetags/gravatar.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.html import escape
 
 import urllib, hashlib
 
@@ -7,6 +8,6 @@ register = template.Library()
 def gravatar(user, size=80):
     gravatar_url = "http://www.gravatar.com/avatar.php?"
     gravatar_url += urllib.urlencode({'gravatar_id':hashlib.md5(user.email).hexdigest(), 'size':str(size)})
-    return """<img src="%s" alt="gravatar for %s" />""" % (gravatar_url, user.username)
+    return """<img src="%s" alt="gravatar for %s" />""" % (escape(gravatar_url), user.username)
 
 register.simple_tag(gravatar)
