103ac1400f1a05149e8bef0db25a429d1abe3baa
James Tauber <jtauber@jtauber.com>
2008-05-31 04:27:14 +0000

    allow for username as arg
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@261 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/gravatar/templatetags/gravatar.py
+++ projects/pinax/gravatar/templatetags/gravatar.py
@@ -1,13 +1,26 @@
 from django import template
 from django.utils.html import escape
+from django.contrib.auth.models import User
 
 import urllib, hashlib
 
 register = template.Library()
 
 def gravatar(user, size=80):
+    if not isinstance(user, User):
+        try:
+            user = User.objects.get(username=user)
+            email = user.email
+            username = user.username
+        except User.DoesNotExist:
+            email = "?"
+            username = user
+    else:
+        email = user.email
+        username = user.username
+        
     gravatar_url = "http://www.gravatar.com/avatar.php?"
-    gravatar_url += urllib.urlencode({'gravatar_id':hashlib.md5(user.email).hexdigest(), 'size':str(size)})
-    return """<img src="%s" alt="gravatar for %s" />""" % (escape(gravatar_url), user.username)
+    gravatar_url += urllib.urlencode({'gravatar_id':hashlib.md5(email).hexdigest(), 'size':str(size)})
+    return """<img src="%s" alt="gravatar for %s" />""" % (escape(gravatar_url), username)
 
 register.simple_tag(gravatar)
