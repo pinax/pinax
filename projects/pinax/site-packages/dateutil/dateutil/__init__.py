b2bcd90bebe19e13c51730a6e9f32fa4c24fc352
James Tauber <jtauber@jtauber.com>
2008-06-05 06:07:59 +0000

    added dateutil
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@305 45601e1e-1555-4799-bd40-45c8c71eef50

---
+++ projects/pinax/site-packages/dateutil/dateutil/__init__.py
@@ -0,0 +1,8 @@
+"""
+Copyright (c) 2003-2005  Gustavo Niemeyer <gustavo@niemeyer.net>
+
+This module offers extensions to the standard python 2.3+
+datetime module.
+"""
+__author__ = "Gustavo Niemeyer <gustavo@niemeyer.net>"
+__license__ = "PSF License"
