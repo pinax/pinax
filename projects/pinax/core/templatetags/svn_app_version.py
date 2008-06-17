8000c870be7c3ed67a2bbb1318c01cd6039b5033
Doug Napoleone <doug.napoleone@gmail.com>
2008-06-17 06:19:40 +0000

    added 'django' special case to svn_app_version
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@370 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/core/templatetags/svn_app_version.py
+++ projects/pinax/core/templatetags/svn_app_version.py
@@ -13,24 +13,26 @@ def svn_app_version(appname=None):
     foo.app {% svn_app_version "foo.app" %}
     project {% svn_app_version %}
     """
-    if not appname:
-        ## RED_FLAG: hard coded relative root!
-        version = cache.get('svn_app_version')
-        if version is None:
-            version = get_svn_revision(dn(dn(dn(abspath(__file__)))))
-            cache.set('svn_app_version', version, 60*60*24*30) # month cache
-        return version
+    cname = 'svn_app_version'
+    if appname: cname += '.' + appname
 
-    version = cache.get('svn_app_version.' + appname)
-    if version is None:
-        if appname not in settings.INSTALLED_APPS: return 'SVN-None'
-        try:
-            module = __import__(appname,{},{},[''])
-        except:
-            if settings.DEBUG:
-                raise
-            version = 'SVN-Error'
+    version = cache.get(cname)
+    if not version:
+        if not appname:
+            ## RED_FLAG: hard coded relative root!
+            version = get_svn_revision(dn(dn(dn(abspath(__file__)))))
+        elif appname == 'django':
+            version = get_svn_revision()
+        elif appname not in settings.INSTALLED_APPS:
+            version = 'SVN-None'
         else:
-            version = get_svn_revision(dn(abspath(module.__file__)))
-        cache.set('svn_app_version.' + appname, version, 60*60*24*30)
+            try:
+                module = __import__(appname,{},{},[''])
+            except:
+                if settings.DEBUG:
+                    raise
+                version = 'SVN-Error'
+            else:
+                version = get_svn_revision(dn(abspath(module.__file__)))
+        cache.set(cname, version, 60*60*24*30)
     return version
