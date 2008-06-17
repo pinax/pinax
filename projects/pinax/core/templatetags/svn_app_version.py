23658f444c05ad5f0cf483d396d7b4c4049d1759
Doug Napoleone <doug.napoleone@gmail.com>
2008-06-17 03:47:06 +0000

    added caching of 1 month to version gets
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@367 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/core/templatetags/svn_app_version.py
+++ projects/pinax/core/templatetags/svn_app_version.py
@@ -1,9 +1,11 @@
 from django import template
 from django.conf import settings
+from django.core.cache import cache
 from os.path import abspath
 from os.path import dirname as dn
 from django.utils.version import get_svn_revision
 register = template.Library()
+
 
 @register.simple_tag
 def svn_app_version(appname=None):
@@ -13,13 +15,22 @@ def svn_app_version(appname=None):
     """
     if not appname:
         ## RED_FLAG: hard coded relative root!
-        return get_svn_revision(dn(dn(dn(abspath(__file__)))))
+        version = cache.get('svn_app_version')
+        if version is None:
+            version = get_svn_revision(dn(dn(dn(abspath(__file__)))))
+            cache.set('svn_app_version', version, 60*60*24*30) # month cache
+        return version
 
-    if appname not in settings.INSTALLED_APPS: return 'SVN-None'
-    try:
-        module = __import__(appname,{},{},[''])
-    except:
-        if settings.DEBUG:
-            raise
-        return 'SVN-Error'
-    return get_svn_revision(dn(abspath(module.__file__)))
+    version = cache.get('svn_app_version.' + appname)
+    if version is None:
+        if appname not in settings.INSTALLED_APPS: return 'SVN-None'
+        try:
+            module = __import__(appname,{},{},[''])
+        except:
+            if settings.DEBUG:
+                raise
+            version = 'SVN-Error'
+        else:
+            version = get_svn_revision(dn(abspath(module.__file__)))
+        cache.set('svn_app_version.' + appname, version, 60*60*24*30)
+    return version
