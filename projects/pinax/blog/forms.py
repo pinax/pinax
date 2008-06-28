b9b4167fdba85a739a2032a8eb7f56a42433bb17
James Tauber <jtauber@jtauber.com>
2008-06-28 08:18:55 +0000

    converted hard tabs and cleaned up unused imports
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@458 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/blog/forms.py
+++ projects/pinax/blog/forms.py
@@ -1,12 +1,9 @@
 from django import newforms as forms
-from django.newforms import widgets
-from django.utils.translation import ugettext_lazy as _
 
 from blog.models import Post
 
 class BlogForm(forms.ModelForm):
-	
-	class Meta:
-		model = Post
-		exclude = ('author', 'creator_ip',
-                   'created_at', 'updated_at', 'publish')
+    
+    class Meta:
+        model = Post
+        exclude = ('author', 'creator_ip', 'created_at', 'updated_at', 'publish')
