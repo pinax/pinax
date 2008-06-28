479395ec86e1755bbc28d19cfd7b68f726aa83e1
James Tauber <jtauber@jtauber.com>
2008-06-28 07:26:24 +0000

    converted hard tabs and added class Admin
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@447 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/blog/models.py
+++ projects/pinax/blog/models.py
@@ -17,7 +17,7 @@ try:
     from django.dispatch import dispatcher
 except ImportError:
     notification = None
-	
+
 try:
     markup_choices = settings.WIKI_MARKUP_CHOICES  # reuse this for now; taken from wiki
 except AttributeError:
@@ -36,7 +36,7 @@ class Post(models.Model):
     title           = models.CharField(_('title'), max_length=200)
     slug            = models.SlugField(_('slug'))
     author          = models.ForeignKey(User, related_name="added_posts", blank=True, null=True)
-    creator_ip 		= models.IPAddressField(_("IP Address of the Article Creator"), blank=True, null=True)
+    creator_ip      = models.IPAddressField(_("IP Address of the Article Creator"), blank=True, null=True)
     body            = models.TextField(_('body'))
     tease           = models.TextField(_('tease'), blank=True)
     status          = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=2)
@@ -44,30 +44,34 @@ class Post(models.Model):
     publish         = models.DateTimeField(_('publish'), default=datetime.now)
     created_at      = models.DateTimeField(_('created at'), default=datetime.now)
     updated_at      = models.DateTimeField(_('updated at'))
-    markup 			= models.CharField(_(u"Article Content Markup"), max_length=3,
+    markup          = models.CharField(_(u"Article Content Markup"), max_length=3,
                               choices=markup_choices,
                               null=True, blank=True)
-    tags 			= TagField()
-
+    tags            = TagField()
+    
     class Meta:
-        verbose_name 		= _('post')
+        verbose_name        = _('post')
         verbose_name_plural = _('posts')
-        ordering  			= ('-publish',)
-        get_latest_by 		= 'publish'
+        ordering            = ('-publish',)
+        get_latest_by       = 'publish'
         unique_together     = ('author', 'slug')
-
+    
     def __unicode__(self):
         return u'%s' % self.title
-
+    
     @permalink
     def get_absolute_url(self):
-		return ('article', None, {
-			'username': self.author.username,
-			'month': self.publish.month,
-			'year': self.publish.year,
-			'slug': self.slug
-		})
-
+        return ('article', None, {
+            'username': self.author.username,
+            'month': self.publish.month,
+            'year': self.publish.year,
+            'slug': self.slug
+    })
+    
     def save(self):
         self.updated_at = datetime.now()
         super(Post, self).save()
+    
+    class Admin:
+        pass
+
