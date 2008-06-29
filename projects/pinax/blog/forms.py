6c6da1aa65f128beebfe3d4a7a7de0ffa6df1ba4
James Tauber <jtauber@jtauber.com>
2008-06-29 09:48:59 +0000

    improved help message
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@475 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/blog/forms.py
+++ projects/pinax/blog/forms.py
@@ -7,7 +7,7 @@ from blog.models import Post
 class BlogForm(forms.ModelForm):
     
     slug = forms.RegexField(max_length=20, regex=r'^\w+$',
-        help_text = _("a short version of the name consisting only of letters, numbers and underscores."),
+        help_text = _("a short version of the title consisting only of letters, numbers and underscores."),
         error_message = _("This value must contain only letters, numbers and underscores."))
     
     class Meta:
