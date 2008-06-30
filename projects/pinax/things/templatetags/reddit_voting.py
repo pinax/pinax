99c53e21d2024477472193c0140502d6fb4f13f6
Alex Gaynor <alex.gaynor@gmail.com>
2008-06-30 00:02:25 +0000

    Added a new sort to django-things based on reddit's algorithm
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@479 45601e1e-1555-4799-bd40-45c8c71eef50

---
+++ projects/pinax/things/templatetags/reddit_voting.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from django import template
+from django.contrib.contenttypes.models import ContentType
+from django.db import connection
+
+from voting.models import Vote
+
+register = template.Library()
+
+def do_order_by_reddit(parser, token):
+    split = token.split_contents()
+    if len(split) == 3:
+        return OrderByRedditNode(*split[1:])
+    else:
+        raise template.TemplateSyntaxError('%r tag takes one argument.' % split[0])
+
+class OrderByRedditNode(template.Node):
+    def __init__(self, queryset_var, date_var):
+        self.queryset_var = template.Variable(queryset_var)
+        self.date_var = date_var
+    
+    def render(self, context):
+        key = self.queryset_var.var
+        values = self.queryset_var.resolve(context)
+        votes = Vote.objects.get_scores_in_bulk([x.id for x in values])
+        ratings = []
+        for obj in values:
+            age = (getattr(values, self.date_var) - datetime(2005, 12, 8, 7, 46, 43)).seconds
+            obj_votes = votes[obj.id]['score']
+            if obj_votes > 0:
+                y = 1
+            elif obj_votes < 0:
+                y = -1
+            else:
+                y = 0
+            z = max(abs(x), 1)
+            
+            rating = log(z, 10) + (y * age) / 45000
+            ratings.append((rating, obj))
+        ratings.sort(cmp=lambda x, y: y[0] - x[0])
+        by_score = [x[1] for x in ratings]
+        
+
+        context[key] = by_score
+        return u""
+
+register.tag('order_by_reddit', do_order_by_reddit)
