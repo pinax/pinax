022a1848005bfc5e2d8f43f73e0add2f8cf18c87
James Tauber <jtauber@jtauber.com>
2008-07-05 09:12:32 +0000

    new approach to ordering
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@525 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/things/templatetags/order_by.py
+++ projects/pinax/things/templatetags/order_by.py
@@ -1,8 +1,27 @@
-from django.template import Library
+from django import template
 
-register = Library()
+register = template.Library()
 
-@register.filter_function
-def order_by(obj, args):
-    args = [x.strip() for x in args.split(',')]
-    return obj.order_by(*args)
+def do_order_by(parser, token):
+    split = token.split_contents()
+    if len(split) == 4:
+        if split[2] == "by":
+            return OrderByNode(split[1], split[3])
+        else:
+            raise template.TemplateSyntaxError('usage is {%% %r x by y %%}' % split[0])
+    else:
+        raise template.TemplateSyntaxError('usage is {%% %r x by y %%}' % split[0])
+
+class OrderByNode(template.Node):
+    def __init__(self, queryset_var, order_field):
+        self.queryset_var = template.Variable(queryset_var)
+        self.order_field = order_field
+        
+    def render(self, context):
+        key = self.queryset_var.var
+        qs = self.queryset_var.resolve(context)
+        
+        context[key] = qs.order_by(self.order_field)
+        return u""
+
+register.tag('order', do_order_by)
