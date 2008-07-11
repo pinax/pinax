c31883a132ddf58578c5af77e210a0bd1f55d411
Jannis Leidel <jannis@leidel.info>
2008-07-11 09:33:29 +0000

    marked notification description translateable
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@552 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/zwitschern/management.py
+++ projects/pinax/zwitschern/management.py
@@ -1,11 +1,12 @@
 from django.dispatch import dispatcher
 from django.db.models import get_models, signals
+from django.utils.translation import ugettext_noop as _
 
 try:
     from notification import models as notification
     
     def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("tweet_follow", "New Tweet Follower", "someone has started following your tweets")
+        notification.create_notice_type("tweet_follow", _("New Tweet Follower"), _("someone has started following your tweets"))
     
     dispatcher.connect(create_notice_types, signal=signals.post_syncdb, sender=notification)
 except ImportError:
