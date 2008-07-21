0d4131ed2116531649be6c0656b7f9ff8cab4fd9
Greg Newman <greg@20seven.org>
2008-07-21 11:58:55 +0000

    connecting photo tags to tag_app to show results
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@630 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/tag_app/views.py
+++ projects/pinax/tag_app/views.py
@@ -3,11 +3,14 @@ from django.template import RequestContext
 
 from blog.models import Post
 from tagging.models import Tag, TaggedItem
+from photos.models import Photos
 
 def tags(request, tag):
     tag = get_object_or_404(Tag, name=tag)
     alltags = TaggedItem.objects.get_by_model(Post, tag)
+    phototags = TaggedItem.objects.get_by_model(Photos, tag)
     return render_to_response('tags/index.html', {
         'tag': tag,
         'alltags': alltags,
+        'phototags': phototags,
     }, RequestContext(request))
