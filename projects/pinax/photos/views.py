a7745060a8efe1bd8c51918b6abbdf8aaa91a2a8
Brian Rosner <brosner@gmail.com>
2008-07-21 06:34:37 +0000

    Added @login_required decorator to all photo views.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@616 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/photos/views.py
+++ projects/pinax/photos/views.py
@@ -27,23 +27,27 @@ def upload(request):
                 return HttpResponseRedirect(reverse("photo_upload"))
 
     return render_to_response("photos/upload.html", {"photo_form": photo_form}, context_instance=RequestContext(request))
-    
+
+@login_required
 def yourphotos(request):
     '''photos for the currently authenticated user'''
     user = request.user
     photos = Photos.objects.filter(member=user)
     return render_to_response("photos/yourphotos.html", {"photos": photos}, context_instance=RequestContext(request))
-    
+
+@login_required    
 def photos(request):
     '''latest photos'''
     photos = Photos.objects.filter(is_public=True)
     return render_to_response("photos/latest.html", {"photos": photos}, context_instance=RequestContext(request))
-    
+
+@login_required
 def details(request, id):
     '''show the photo details'''
     photo = get_object_or_404(Photos, id=id)
     return render_to_response("photos/details.html", {"photo": photo}, context_instance=RequestContext(request))
     
+@login_required
 def memberphotos(request, username):
     '''Get the members photos and display them'''
     user = get_object_or_404(User, username=username)
