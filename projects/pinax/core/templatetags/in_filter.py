4258a22a4ad7a1b19f2c29d70584664081d9b7eb
Brian Rosner <brosner@gmail.com>
2008-07-26 01:54:02 +0000

    Added a docstring to the in_list template filter.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@675 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/pinax/core/templatetags/in_filter.py
+++ projects/pinax/core/templatetags/in_filter.py
@@ -5,5 +5,8 @@ register = template.Library()
 # http://www.djangosnippets.org/snippets/379/
 
 @register.filter
-def in_list(value,arg):
-   return value in arg
+def in_list(value, arg):
+    """
+    Tests if value is in arg.
+    """
+    return value in arg
