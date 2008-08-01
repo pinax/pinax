a086ce666e12213b7f621eac2d49acd913d11a9a
James Tauber <jtauber@jtauber.com>
2008-08-01 19:02:46 +0000

    added inclusion tag for tags with counts
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@700 45601e1e-1555-4799-bd40-45c8c71eef50

--- pinax/tag_app/templatetags/extra_tagging_tags.py
+++ pinax/tag_app/templatetags/extra_tagging_tags.py
@@ -5,3 +5,7 @@ register = Library()
 @register.inclusion_tag("tag_app/tag_list.html")
 def show_tags_for(obj):
     return {"obj": obj}
+
+@register.inclusion_tag("tag_app/tag_count_list.html")
+def show_tag_counts(tag_counts):
+    return {"tag_counts": tag_counts}
