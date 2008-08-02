eb3a97bac18dedf6cb200e53b464361ecc02d561
Eduardo O. Padoan <eduardo.padoan@gmail.com>
2008-08-02 02:42:09 +0000

    Adding tags to wiki articles -- work in progress...
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@711 45601e1e-1555-4799-bd40-45c8c71eef50

--- pinax/tag_app/views.py
+++ pinax/tag_app/views.py
@@ -11,19 +11,23 @@ from projects.models import Topic as ProjectTopic
 from tribes.models import Tribe
 from tribes.models import Topic as TribeTopic
 
+from wiki.models import Article as WikiArticle
 
 def tags(request, tag):
     tag = get_object_or_404(Tag, name=tag)
     alltags = TaggedItem.objects.get_by_model(Post, tag)
     phototags = TaggedItem.objects.get_by_model(Photos, tag)
-    
+
     project_tags = TaggedItem.objects.get_by_model(Project, tag)
     project_topic_tags = TaggedItem.objects.get_by_model(ProjectTopic, tag)
     project_task_tags = TaggedItem.objects.get_by_model(Task, tag)
-    
+
     tribe_tags = TaggedItem.objects.get_by_model(Tribe, tag)
     tribe_topic_tags = TaggedItem.objects.get_by_model(TribeTopic, tag)
-    
+
+    # @@@ TODO: tribe_wiki_article_tags and project_wiki_article_tags
+    wiki_article_tags = TaggedItem.objects.get_by_model(WikiArticle, tag)
+
     return render_to_response('tags/index.html', {
         'tag': tag,
         'alltags': alltags,
@@ -33,4 +37,5 @@ def tags(request, tag):
         'project_task_tags': project_task_tags,
         'tribe_tags': tribe_tags,
         'tribe_topic_tags': tribe_topic_tags,
+        'wiki_article_tags': wiki_article_tags,
     }, RequestContext(request))
