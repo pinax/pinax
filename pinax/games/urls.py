e9cd7ecf32141a82201fa4b5092bb813d29d43e7
Eric Florenzano <floguy@gmail.com>
2008-08-02 23:33:26 +0000

    Added categories, basic sorting, and made the game list look a lot better.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@723 45601e1e-1555-4799-bd40-45c8c71eef50

--- pinax/games/urls.py
+++ pinax/games/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('games',
     url(r'^game/(?P<slug>[a-zA-Z0-9_-]+)/$', 'views.game_detail', name='games_detail'),
+    url(r'^category/(?P<category>[a-zA-Z0-9_-]+)/$', 'views.game_list', name='games_category'),
+    url(r'^category/(?P<category>[a-zA-Z0-9_-]+)/(?P<sort>[a-z-]+)/$', 'views.game_list', name='games_category_list'),
+    url(r'^categories/$', 'views.categories', name='games_categories'),
+    url(r'^(?P<sort>[a-z-]+)/$', 'views.game_list', name='games_list'),
     url(r'^$', 'views.game_list', name='games_index'),
 )
--- pinax/games/views.py
+++ pinax/games/views.py
@@ -1,18 +1,42 @@
-from arcade.models import Game
+from arcade.models import Game, ArcadeCategory
 
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
-def game_list(request):
+def game_list(request, sort="most-played", category=None):
+    games = Game.objects.filter(downloaded=True)
+    if category is not None:
+        category = get_object_or_404(ArcadeCategory, slug=category.lower())
+        games = category.game_set.filter(downloaded=True)
+    if sort == 'most-played':
+        games = games.order_by('-game_plays')
+    elif sort == 'alphabetical':
+        games = games.order_by('name')
+    elif sort == 'newest':
+        games = games.order_by('-created')
     context = {
-        'games': Game.objects.filter(downloaded=True),
+        'games': games,
+        'sort': sort,
+        'category': category,
     }
     return render_to_response('games/game_list.html',
         context, context_instance=RequestContext(request))
 
-def game_detail(request, slug=''):
+def categories(request):
+    cats = ArcadeCategory.objects.all()
+    cats = [(c, c.game_set.filter(downloaded=True).count()) for c in cats]
     context = {
-        'game': get_object_or_404(Game, slug=slug.lower()),
+        'categories': cats,
+    }
+    return render_to_response('games/category_list.html',
+        context, context_instance=RequestContext(request))
+
+def game_detail(request, slug=''):
+    game = get_object_or_404(Game, slug=slug.lower())
+    if request.META['HTTP_REFERER'] != request.path:
+        game.add_play()
+    context = {
+        'game': game,
     }
     return render_to_response('games/game_detail.html',
         context, context_instance=RequestContext(request))
