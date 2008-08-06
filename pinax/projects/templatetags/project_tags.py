9b32a0554f4fef784d4eec0ea31c268d313ffcfd
James Tauber <jtauber@jtauber.com>
2008-08-06 15:28:38 +0000

    fixed stupid error where I thought it would be okay to use tribe inclusion template for project
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@733 45601e1e-1555-4799-bd40-45c8c71eef50

--- pinax/projects/templatetags/project_tags.py
+++ pinax/projects/templatetags/project_tags.py
@@ -2,8 +2,7 @@ from django.template import Library
 
 register = Library()
 
-# @@@ for now let's favour DRY over loose coupling and reference tribes
-@register.inclusion_tag("tribes/topic_item.html")
+@register.inclusion_tag("projects/topic_item.html")
 def show_project_topic(topic):
     return {"topic": topic}
 
