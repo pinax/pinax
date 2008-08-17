3f7760ee453ce4b93ee115a6a4f709d1c3202f83
Greg Newman <greg@20seven.org>
2008-08-17 11:59:27 +0000

    added pownce_utils.py so lifestreams would function.  commented out url in lifestreams so site would function until beshr gets it finished
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@768 45601e1e-1555-4799-bd40-45c8c71eef50

--- pinax/lifestream/urls.py
+++ pinax/lifestream/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^personal/$', 'lifestream.views.personal'),
-    (r'^friends/$', 'lifestream.views.friends'),
+   # (r'^friends/$', 'lifestream.views.friends'),
     (r'^(\w+)/$', 'lifestream.views.single'),
 )
