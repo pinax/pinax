c2197f251ef6afe8089d8c12ba0879af4fcf6a93
Greg Newman <greg@20seven.org>
2008-08-17 16:26:40 +0000

    added template tag for photos to process exif
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@778 45601e1e-1555-4799-bd40-45c8c71eef50

---
+++ pinax/photos/templatetags/photo_tags.py
@@ -0,0 +1,38 @@
+import re
+from django import template
+from photologue.utils import EXIF
+register = template.Library()
+
+@register.tag(name="print_exif")
+def do_print_exif(parser, token):
+    try:
+        tag_name, exif = token.contents.split()
+    except ValueError:
+        msg = '%r tag requires a single argument' % token.contents[0]
+        raise template.TemplateSyntaxError(msg)
+
+    exif = parser.compile_filter(exif)
+    return PrintExifNode(exif)
+
+class PrintExifNode(template.Node):
+
+    def __init__(self, exif):
+
+        self.exif = exif
+
+    def render(self, context):
+        try:
+            exif = unicode(self.exif.resolve(context, True))
+        except template.VariableDoesNotExist:
+            exif = u''
+
+        EXPR =  "'(?P<key>[^:]*)'\:(?P<value>[^,]*),"
+        expr = re.compile(EXPR)
+        msg  = "<table>"
+        for i in expr.findall(exif):
+            msg += "<tr><td>%s</td><td>%s</td></tr>" % (i[0],i[1])
+
+        msg += "</table>"
+
+        return u'<div id="exif">%s</div>' % (msg)
+
