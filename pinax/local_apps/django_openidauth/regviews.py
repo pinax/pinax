c319a5f7048b138049da011308c1bf1a9296475b
Doug Napoleone <doug.napoleone@gmail.com>
2008-09-04 14:52:13 +0000

    fix for profile user duplicate key crash. NOTE TO CLIENTS: those who were affected by this crash may have corrupted DB's. You need to run the create_profiles_for_users management command to fix.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@842 45601e1e-1555-4799-bd40-45c8c71eef50

--- pinax/local_apps/django_openidauth/regviews.py
+++ pinax/local_apps/django_openidauth/regviews.py
@@ -17,7 +17,7 @@ alnum_re = re.compile(r'^\w+$')
 class RegistrationFormOpenID(forms.Form):
     username = forms.CharField(label="Username", max_length=30, widget=forms.TextInput())
     email = forms.EmailField(label="Email (optional)", required=False, widget=forms.TextInput())
-    
+
     def clean_username(self):
         if not alnum_re.search(self.cleaned_data["username"]):
             raise forms.ValidationError(u"Usernames can only contain letters, numbers and underscores.")
@@ -26,58 +26,60 @@ class RegistrationFormOpenID(forms.Form):
         except User.DoesNotExist:
             return self.cleaned_data["username"]
         raise forms.ValidationError(u"This username is already taken. Please choose another.")
-    
+
     def save(self):
         username = self.cleaned_data["username"]
         email = self.cleaned_data["email"]
         new_user = User.objects.create_user(username, "", "!")
-        Profile(user=new_user).save()
+        
+        profile, created = Profile.objects.get_or_create(user=new_user)
+        if created: profile.save()
         if email:
             new_user.message_set.create(message="Confirmation email sent to %s" % email)
             EmailAddress.objects.add_email(new_user, email)
         return new_user
 
-def register(request, success_url='/accounts/register/complete/', 
+def register(request, success_url='/accounts/register/complete/',
         template_name='openid_register.html', already_registered_url='/'):
     """
-    Allows a new user to register an account. A customised variation of the 
+    Allows a new user to register an account. A customised variation of the
     view of the same name from django-registration.
 
     Context::
         form
             The registration form
-    
+
     Template::
         registration/registration_form.html (or template_name argument)
-    
+
     """
     if request.method == 'POST':
         form = RegistrationFormOpenID(request.POST)
-        
+
         if form.is_valid():
             new_user = form.save()
             associate_openid( new_user, request.openid )
-            
-            # Unfortunately we have to annotate the user with the 
+
+            # Unfortunately we have to annotate the user with the
             # 'django.contrib.auth.backends.ModelBackend' backend, or stuff breaks
             backend = load_backend('django.contrib.auth.backends.ModelBackend')
             new_user.backend = '%s.%s' % (
                 backend.__module__, backend.__class__.__name__
             )
             log_user_in(request, new_user)
-            
+
             return HttpResponseRedirect(success_url)
     else:
         if request.user.is_authenticated():
             return HttpResponseRedirect( already_registered_url )
         form = RegistrationFormOpenID()
-    
+
     return render_to_response(template_name, { 'form': form },
                               context_instance=RequestContext(request))
 
 def demo_delete_me_asap(request):
     import django.forms
-    
+
     class UserProfileForm(forms.Form):
         name = forms.CharField(max_length=100)
         email = forms.EmailField()
