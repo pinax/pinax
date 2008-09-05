5ede77217213a9aeded676c975cf47f2eaa530fa
Greg Newman <greg@20seven.org>
2008-09-05 11:24:26 +0000

    adding footer context processor patch provided by ericflo
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@846 45601e1e-1555-4799-bd40-45c8c71eef50

--- pinax/local_apps/core/context_processors.py
+++ pinax/local_apps/core/context_processors.py
@@ -5,3 +5,12 @@ def contact_email(request):
 
 def site_name(request):
     return {'site_name': getattr(settings, 'SITE_NAME', '')}
+
+def footer(request): 
+ 	return {
+        'latest_tweets': Tweet.objects.all().order_by('-sent')[:5], 
+        'latest_tribes': Tribe.objects.all().order_by('-created')[:5], 
+        'latest_users': User.objects.all().order_by('-date_joined')[:5], 
+        'latest_bookmarks': Bookmark.objects.all().order_by('-added')[:5],
+    }
+
