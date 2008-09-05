adbe75223fc68f2520186030e7d99b4bab4673b1
Greg Newman <greg@20seven.org>
2008-09-05 23:16:15 +0000

    foadding blogs to the context filter for footers; latest 5 blogs
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@848 45601e1e-1555-4799-bd40-45c8c71eef50

--- pinax/local_apps/core/context_processors.py
+++ pinax/local_apps/core/context_processors.py
@@ -3,6 +3,7 @@ from zwitschern.models import Tweet
 from tribes.models import Tribe 
 from django.contrib.auth.models import User 
 from bookmarks.models import Bookmark
+from blog.models import Post
 
 
 def contact_email(request):
@@ -17,5 +18,6 @@ def footer(request):
         'latest_tribes': Tribe.objects.all().order_by('-created')[:5], 
         'latest_users': User.objects.all().order_by('-date_joined')[:5], 
         'latest_bookmarks': Bookmark.objects.all().order_by('-added')[:5],
+        'latest_blogs': Post.objects.filter(status=2).order_by('-publish')[:5],
     }
 
