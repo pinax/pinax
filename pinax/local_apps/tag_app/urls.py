6e4137b7fe50003e9e62742ff62bd86c8992352a
James Tauber <jtauber@jtauber.com>
2008-09-13 06:35:52 +0000

    django-tagging allows spaces in tags so this url should too
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@880 45601e1e-1555-4799-bd40-45c8c71eef50

--- pinax/local_apps/tag_app/urls.py
+++ pinax/local_apps/tag_app/urls.py
@@ -5,5 +5,5 @@ from tag_app import views
 
 urlpatterns = patterns('',
     # all tags
-	url(r'^(?P<tag>\w+)/$', 'tag_app.views.tags', name='tag_results'),
+	url(r'^(?P<tag>.+)/$', 'tag_app.views.tags', name='tag_results'),
 )
