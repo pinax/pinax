c8eafbb87b652545fdb6022cedb6f525a9d40d61
Greg Newman <greg@20seven.org>
2008-09-20 15:52:21 +0000

    adding django-flag to photo details
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@905 45601e1e-1555-4799-bd40-45c8c71eef50

--- pinax/settings.py
+++ pinax/settings.py
@@ -140,6 +140,7 @@ INSTALLED_APPS = (
     'arcade',
     'games',
     'locations',
+    'flag',
     
     # internal (for now)
     'analytics',
--- pinax/urls.py
+++ pinax/urls.py
@@ -55,6 +55,7 @@ urlpatterns = patterns('',
     (r'^games/', include('games.urls')),
     (r'^swaps/', include('swaps.urls')),
     (r'^locations/', include('locations.urls')),
+    (r'^flag/', include('flag.urls')),
     
     (r'^feeds/tweets/(.*)/$', 'django.contrib.syndication.views.feed', tweets_feed_dict),
     (r'^feeds/posts/(.*)/$', 'django.contrib.syndication.views.feed', blogs_feed_dict),
