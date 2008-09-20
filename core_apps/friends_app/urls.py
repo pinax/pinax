9e24bf0e88f8c6a9d6f93262baedba097a036bdc
James Tauber <jtauber@jtauber.com>
2008-09-20 17:02:55 +0000

    named invitations url conf
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@907 45601e1e-1555-4799-bd40-45c8c71eef50

--- core_apps/friends_app/urls.py
+++ core_apps/friends_app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^$', 'friends_app.views.friends'),
+    url(r'^$', 'friends_app.views.friends', name="invitations"),
     url(r'^contacts/$', 'friends_app.views.contacts'),
     url(r'^accept/(\w+)/$', 'friends_app.views.accept_join', name='friends_accept_join'),
 )
