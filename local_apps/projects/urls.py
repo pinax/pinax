50f15cde8e05f50633e4cc0b2d717842a8e79a54
Eduardo O. Padoan <eduardo.padoan@gmail.com>
2008-09-21 22:00:02 +0000

    Disallow non-members from editing wiki Articles on projects.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@922 45601e1e-1555-4799-bd40-45c8c71eef50

--- local_apps/projects/urls.py
+++ local_apps/projects/urls.py
@@ -8,27 +8,28 @@ pt = ProjectThing(Project)
 
 wiki_args = {
     'group_slug_field': 'slug',
-    'group_qs': Project.objects.all()
+    'group_qs': Project.objects.all(),
+    'is_member': (lambda user, group: group.has_member(user)),
+    'is_private': (lambda group: group.private),
 }
-
 
 urlpatterns = pt.urls(url_prefix='', name_prefix='project_thing') + patterns('',
     url(r'^create/$', 'projects.views.create', name="project_create"),
     # @@@ what to do about clash with project with slug 'your_projects'?
     url(r'^your_projects/$', 'projects.views.your_projects', name="your_projects"),
     url(r'^project/(\w+)/$', 'projects.views.project', name="project_detail"),
-    
+
     url(r'^project/(\w+)/members_status/$', 'projects.views.members_status', name="project_members_status"),
-    
+
     # topics
     url(r'^project/(\w+)/topics/$', 'projects.views.topics', name="project_topics"),
     url(r'^topic/(\d+)/$', 'projects.views.topic', name="project_topic"),
-    
+
     # tasks
     url(r'^project/(\w+)/tasks/$', 'projects.views.tasks', name="project_tasks"),
     url(r'^task/(\d+)/$', 'projects.views.task', name="project_task"),
     url(r'^tasks/(\w+)/$', 'projects.views.user_tasks', name="project_user_tasks"),
-    
+
     # wiki
     url(r'^project/(?P<group_slug>\w+)/wiki/', include('wiki.urls'), kwargs=wiki_args),
 )
