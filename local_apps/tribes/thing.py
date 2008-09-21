5214aeeab67719d3b42e64b2d2c182bb917cce6d
Eric Florenzano <floguy@gmail.com>
2008-09-21 05:05:34 +0000

    Forgot to svn add a bunch of files.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@913 45601e1e-1555-4799-bd40-45c8c71eef50

---
+++ local_apps/tribes/thing.py
@@ -0,0 +1,12 @@
+import things
+from tribes.models import Tribe
+
+class TribeThing(things.ModelThing):
+    created = things.OrderField(verbose_name_asc='Newest', 
+        verbose_name_desc='Oldest', url_asc='newest', url_desc='oldest', 
+        field_url='date')
+    name = things.OrderField()
+    members = things.OrderField(verbose_name_asc='Largest', 
+        verbose_name_desc='Smallest', url_asc='largest', url_desc='smallest',
+        field_url='size')
+    search = ('name', 'description')
