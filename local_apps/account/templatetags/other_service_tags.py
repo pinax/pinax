dfaa1f26c905ee0aad97e805e4c0cddceebd6b9d
James Tauber <jtauber@jtauber.com>
2008-09-27 12:34:56 +0000

    other_service tag now supports assignment to variable
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@948 45601e1e-1555-4799-bd40-45c8c71eef50

--- local_apps/account/templatetags/other_service_tags.py
+++ local_apps/account/templatetags/other_service_tags.py
@@ -1,12 +1,45 @@
+import re
+
 from django import template
 from account.models import OtherServiceInfo
 
 register = template.Library()
 
-def other_service(user, key):
-    try:
-        value = OtherServiceInfo.objects.get(user=user, key=key).value
-    except OtherServiceInfo.DoesNotExist:
-        value = u""
-    return value
-register.simple_tag(other_service)
+class OtherServiceNode(template.Node):
+    def __init__(self, user, key, asvar):
+        self.user = user
+        self.key = key
+        self.asvar = asvar
+    
+    def render(self, context):
+        user = self.user.resolve(context)
+        key = self.key
+        try:
+            value = OtherServiceInfo.objects.get(user=user, key=key).value
+        except OtherServiceInfo.DoesNotExist:
+            value = ''
+                    
+        if self.asvar:
+            context[self.asvar] = value
+            return ''
+        else:
+            return value
+
+
+@register.tag
+def other_service(parser, token):
+    bits = token.contents.split(' ')
+    if len(bits) == 3: # {% other_service user key %}
+        user = parser.compile_filter(bits[1])
+        key = bits[2]
+        asvar = None
+    elif len(bits) == 5: # {% other_service user key as var %}
+        if bits[3] != "as":
+            raise template.TemplateSyntaxError("3rd argument to %s should be 'as'" % bits[0])
+        user = parser.compile_filter(bits[1])
+        key = bits[2]
+        asvar = bits[4]
+    else:
+        raise template.TemplateSyntaxError("wrong number of arguments to %s" % bits[0])
+    
+    return OtherServiceNode(user, key, asvar)
