e3415fbb100f30358ceac64dd7eb691a82d88ad9
Eric Florenzano <floguy@gmail.com>
2008-10-06 01:42:19 +0000

    Added a group listing to the context of the games page, for use if wanted in other projects.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1023 45601e1e-1555-4799-bd40-45c8c71eef50

--- apps/core_apps/games/urls.py
+++ apps/core_apps/games/urls.py
@@ -5,5 +5,9 @@ from games.thing import GameThing, GameThingGroup, group_dicts
 thing = GameThingGroup(GameThing, group_func=group_dicts)
 
 urlpatterns = thing.urls(name_prefix='games') + patterns('games',
-    url(r'^game/(?P<slug>[a-zA-Z0-9_-]+)/$', 'views.game_detail', name='game_detail'),
+    url(
+        r'^game/(?P<slug>[a-zA-Z0-9_-]+)/$', 
+        'views.game_detail',
+        {'extra_context': {'groups': thing.groups}},
+        name='game_detail'),
 )
--- apps/core_apps/games/views.py
+++ apps/core_apps/games/views.py
@@ -3,10 +3,12 @@ from arcade.models import Game, ArcadeCategory
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
-def game_detail(request, slug='', template_name='games/game_detail.html'):
+def game_detail(request, slug='', template_name='games/game_detail.html',
+    extra_context={}):
     game = get_object_or_404(Game, slug=slug.lower())
     if request.META.get('HTTP_REFERER', '') != request.path:
         game.add_play()
-    return render_to_response(template_name, {
-        'game': game,
-    }, context_instance=RequestContext(request))
+    context = {'game': game}
+    context.update(extra_context)
+    return render_to_response(template_name, context, 
+        context_instance=RequestContext(request))
