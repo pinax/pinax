ec774b88c1b7ff2fb651bda984744c382337bb83
James Tauber <jtauber@jtauber.com>
2008-10-15 19:25:49 +0000

    updated doc conf with new revision number
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1054 45601e1e-1555-4799-bd40-45c8c71eef50

--- docs/conf.py
+++ docs/conf.py
@@ -42,9 +42,9 @@ copyright = '2008, James Tauber and Pinax Team'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.5'
 # The full version, including alpha/beta/rc tags.
-release = '0.1dev'
+release = '0.5beta'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
