a9c6d6e1bcc8b70ec853bc32d3498aa29f328016
Greg Newman <greg@20seven.org>
2008-10-16 20:50:11 +0000

    fixing broken url for ajax autocomplete
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1069 45601e1e-1555-4799-bd40-45c8c71eef50

--- apps/local_apps/profiles/urls.py
+++ apps/local_apps/profiles/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+    url(r'^username_autocomplete/$', 'profiles.views.username_autocomplete', name='profile_username_autocomplete'),
     url(r'^$', 'profiles.views.profiles', name='profile_list'),
     url(r'^(?P<username>[\w]+)/$', 'profiles.views.profile', name='profile_detail'),
-    url(r'^username_autocomplete/$', 'profiles.views.username_autocomplete', name='profile_username_autocomplete'),
 )
