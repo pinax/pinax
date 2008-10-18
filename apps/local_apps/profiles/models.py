2cbaf7b163c921badbfed4575240531ae290d1e9
James Tauber <jtauber@jtauber.com>
2008-10-18 19:49:37 +0000

    removed deprecated profile fields
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1088 45601e1e-1555-4799-bd40-45c8c71eef50

--- apps/local_apps/profiles/models.py
+++ apps/local_apps/profiles/models.py
@@ -7,29 +7,20 @@ from django.utils.translation import ugettext_lazy as _
 from timezones.fields import TimeZoneField
 
 class Profile(models.Model):
-
+    
     user = models.ForeignKey(User, unique=True, verbose_name=_('user'))
     name = models.CharField(_('name'), max_length=50, null=True, blank=True)
     about = models.TextField(_('about'), null=True, blank=True)
     location = models.CharField(_('location'), max_length=40, null=True, blank=True)
     website = models.URLField(_('website'), null=True, blank=True, verify_exists=False)
     
-    # @@@ the following are all deprecated -  see account/models.py
-    blogrss = models.URLField(_('blog rss/atom'), null=True, blank=True, verify_exists=False)
-    timezone = TimeZoneField(_('timezone'))
-    twitter_user = models.CharField(_('Twitter Username'), max_length=50, blank=True)
-    twitter_password = models.CharField(_('Twitter Password'), max_length=50, blank=True)
-    pownce_user = models.CharField(_('Pownce Username'), max_length=50, blank=True)
-    pownce_password = models.CharField(_('Pownce Password'), max_length=50, blank=True)
-    language = models.CharField(_('language'), max_length=10, choices=settings.LANGUAGES, default=settings.LANGUAGE_CODE)
-
     def __unicode__(self):
         return self.user.username
-
+    
     def get_absolute_url(self):
         return ('profile_detail', None, {'username': self.user.username})
     get_absolute_url = models.permalink(get_absolute_url)
-
+    
     class Meta:
         verbose_name = _('profile')
         verbose_name_plural = _('profiles')
