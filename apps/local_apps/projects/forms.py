9e4db141226c30c1f6205f921304e08891850900
James Tauber <jtauber@jtauber.com>
2008-10-27 10:40:32 +0000

    added missing imports - cgc ticket #97; Forward port of r1159 from 0.5.X branch
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1160 45601e1e-1555-4799-bd40-45c8c71eef50

--- apps/local_apps/projects/forms.py
+++ apps/local_apps/projects/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
+from django.contrib.auth.models import User
+try:
+    from notification import models as notification
+except ImportError:
+    notification = None
+
 from projects.models import Project, Topic, Task, ProjectMember
 
 # @@@ this is based on Tribes -- can we re-use anything?
