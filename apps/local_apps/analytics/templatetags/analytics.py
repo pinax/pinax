13c951d34e7978d9f820ecd556d923dd7842e114
James Tauber <jtauber@jtauber.com>
2008-10-31 12:20:11 +0000

    patch for new google analytics codes -- contributed by kjellmf
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1189 45601e1e-1555-4799-bd40-45c8c71eef50

--- apps/local_apps/analytics/templatetags/analytics.py
+++ apps/local_apps/analytics/templatetags/analytics.py
@@ -16,4 +16,23 @@ def urchin():
     else:
         return ""
 
+def ga():
+    # Use new Google Analytics tracking code
+    urchin_id = getattr(settings, 'URCHIN_ID', None)
+    if urchin_id:
+        return """
+    <script type="text/javascript">
+        var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+        document.write(unescape("%%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%%3E%%3C/script%%3E"));
+    </script>
+    <script type="text/javascript">
+        var pageTracker = _gat._getTracker("%s");
+        pageTracker._trackPageview();
+    </script>
+    """ % settings.URCHIN_ID
+    else:
+        return ""
+
+
 register.simple_tag(urchin)
+register.simple_tag(ga)
