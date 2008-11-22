c55f5a5b532efe44d99ea2d95e0f58d18452783f
Brian Rosner <brosner@gmail.com>
2008-11-22 03:30:49 +0000

    Implemented the django_openid patch locally and removed the patch file.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1213 45601e1e-1555-4799-bd40-45c8c71eef50

--- apps/local_apps/account/openid_consumer.py
+++ apps/local_apps/account/openid_consumer.py
@@ -1,6 +1,8 @@
 
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 from django_openid.registration import RegistrationConsumer
 
@@ -16,3 +18,11 @@ class PinaxConsumer(RegistrationConsumer):
     
     def get_registration_form_class(self, request):
         return OpenIDSignupForm
+    
+    def render(self, request, template, context=None):
+        # TODO: remove this method. this method is re-implemented to fix a
+        # http://code.google.com/p/django-openid/issues/detail?id=22
+        context = context or {}
+        context['base_template'] = self.base_template
+        return render_to_response(template, context,
+            context_instance=RequestContext(request))
