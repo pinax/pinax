4e5ab79cc82c90c9a5572cbe9531d5d38a01a161
Eric Florenzano <floguy@gmail.com>
2008-12-01 08:13:12 +0000

    Greatly reduced the amount of friends generated.  This is OK because it's a bidirectional relationship so even after the user's friends have been added, more are added still.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1242 45601e1e-1555-4799-bd40-45c8c71eef50

--- fixtures/generate/gen_friends.py
+++ fixtures/generate/gen_friends.py
@@ -9,7 +9,7 @@ def generate():
     num_users = User.objects.all().count()
     for user in User.objects.all():
         pre_friend_ids = [i['friend'].id for i in Friendship.objects.friends_for_user(user)]
-        num = random.randint(0, num_users - 1)
+        num = random.randint(1, (num_users - 1) / 10)
         friends = User.objects.exclude(id__in=pre_friend_ids).order_by('?')[:num]
         for friend in friends:
             num_words = random.randint(1, 100)
