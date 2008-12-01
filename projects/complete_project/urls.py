737ecadf3c97bd3b38a90edf112848ecbe29bb3c
Brian Rosner <brosner@gmail.com>
2008-12-01 00:06:21 +0000

    Implemented the necessary changes to complete_project to work with the microblogging generic foreign key change. Thanks Jay Graves.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1237 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/complete_project/urls.py
+++ projects/complete_project/urls.py
@@ -80,7 +80,7 @@ from microblogging.models import Tweet
 
 friends_tweets_kwargs = {
     "template_name": "microblogging/friends_tweets.html",
-    "friends_objects_function": lambda users: Tweet.objects.filter(sender__in=users),
+    "friends_objects_function": lambda users: Tweet.objects.filter(sender_id__in=[user.id for user in users], sender_type__name='user'),
 }
 
 from bookmarks.models import Bookmark
