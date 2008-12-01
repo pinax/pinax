4093688dfcec2dfc1aca152f42e29a4fc1ef2c45
Eric Florenzano <floguy@gmail.com>
2008-12-01 08:11:11 +0000

    Added support for more possible username chars.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1239 45601e1e-1555-4799-bd40-45c8c71eef50

--- apps/local_apps/profiles/urls.py
+++ apps/local_apps/profiles/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^username_autocomplete/$', 'profiles.views.username_autocomplete', name='profile_username_autocomplete'),
     url(r'^$', 'profiles.views.profiles', name='profile_list'),
-    url(r'^(?P<username>[\w]+)/$', 'profiles.views.profile', name='profile_detail'),
+    url(r'^(?P<username>[\w\._-]+)/$', 'profiles.views.profile', name='profile_detail'),
 )
