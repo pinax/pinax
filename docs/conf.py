4cccac7917f0cc17ed6cb56f5beab50baa646e12
Brian Rosner <brosner@gmail.com>
2008-12-12 17:14:19 +0000

    [0.5.X] Reverted r1251 for the same reasons as r1276.
    git-svn-id: http://svn.pinaxproject.com/pinax/branches/0.5.X@1277 45601e1e-1555-4799-bd40-45c8c71eef50

--- docs/conf.py
+++ docs/conf.py
@@ -41,11 +41,10 @@ copyright = '2008, James Tauber and Pinax Team'
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
 #
-
-# The full version, including alpha/beta/rc tags.
-release = open(os.path.join('..', 'VERSION')).read()
 # The short X.Y version.
-version = release[:release.index('.', 2)]
+version = '0.5'
+# The full version, including alpha/beta/rc tags.
+release = '0.5.1dev'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
