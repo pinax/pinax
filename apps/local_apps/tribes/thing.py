7d626d1d4316bd6d2ed4a934bc73c1f128d9f353
Brian Rosner <brosner@gmail.com>
2008-12-14 05:24:39 +0000

    [0.5.X] Marked some strings for translation in tribes/thing.py. Thanks msaelices for the patch.
    Backport of r1278 from trunk.
    
    git-svn-id: http://svn.pinaxproject.com/pinax/branches/0.5.X@1279 45601e1e-1555-4799-bd40-45c8c71eef50

--- apps/local_apps/tribes/thing.py
+++ apps/local_apps/tribes/thing.py
@@ -1,29 +1,30 @@
 import things
 from django.db import connection
+from django.utils.translation import ugettext_lazy as _
 
 class TribeThing(things.Thing):
     created = things.OrderField(
-        verbose_name_asc='Age', 
-        verbose_name_desc='Age', 
+        verbose_name_asc=_('Age'), 
+        verbose_name_desc=_('Age'), 
         url_asc='oldest', 
         url_desc='newest', 
         field_url='date',
         reverse=True
     )
     name = things.OrderField(
-        verbose_name_asc='Name', 
-        verbose_name_desc='Name'
+        verbose_name_asc=_('Name'), 
+        verbose_name_desc=_('Name')
     )
     members = things.OrderCountField(
-        verbose_name_asc='Member Count', 
-        verbose_name_desc='Member Count', 
+        verbose_name_asc=_('Member Count'), 
+        verbose_name_desc=_('Member Count'), 
         url_asc='least-members', 
         url_desc='most-members', 
         field_url='members'
     )
     topics = things.OrderCountField(
-        verbose_name_asc='Topic Count', 
-        verbose_name_desc='Topic Count', 
+        verbose_name_asc=_('Topic Count'), 
+        verbose_name_desc=_('Topic Count'), 
         url_asc='least-topics', 
         url_desc='most-topics', 
         field_url='topics'
