1021f08d85cef05603e19af29b918811c870a801
Eric Florenzano <floguy@gmail.com>
2009-01-10 07:08:43 +0000

    Added one-sentence summaries to the __about__ attribute in each project's __init__.py
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1310 45601e1e-1555-4799-bd40-45c8c71eef50

---
+++ projects/complete_project/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__about__ = """
+This project comes fully-featured, with everything that Pinax provides enabled
+by default. It provides all tabs available, etc. From here you can remove
+applications that you do not want to use, and add your own applications as well.
+"""
---
+++ projects/content_project/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__about__ = """
+This project comes with a base set of apps that are good for building a content
+management system.  If you're looking to start a mainly content-driven web site,
+this is the project to choose.
+"""
