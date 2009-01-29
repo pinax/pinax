8fb18c1eb4e166b5741524875d0ce5e7152f7dca
James Tauber <jtauber@jtauber.com>
2009-01-29 23:53:53 +0000

    put atomformat in a package with setup.py
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1455 45601e1e-1555-4799-bd40-45c8c71eef50

---
+++ libs/atom-format/README.txt
@@ -0,0 +1,1 @@
+Note: this is just the atom.py from django-atompub, renamed as atomformat.py
---
+++ libs/atom-format/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='django-atompub',
+    version='0.1.0dev',
+    author='James Tauber',
+    url='http://code.google.com/p/django-atompub/',
+    py_modules=['atomformat'],
+)
