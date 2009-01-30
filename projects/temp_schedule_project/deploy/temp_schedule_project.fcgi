0a8ffe7c5d8a7619cef85fde1d36f1413974e4d3
James Tauber <jtauber@jtauber.com>
2009-01-30 02:15:03 +0000

    first pass at making django-schedule's project_sample into a starter project for pinax (albeit a temporary one)
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1460 45601e1e-1555-4799-bd40-45c8c71eef50

---
+++ projects/temp_schedule_project/deploy/temp_schedule_project.fcgi
@@ -0,0 +1,23 @@
+# basic_project.fcgi is configured to live in projects/basic_project/deploy.
+
+import os
+import sys
+
+from os.path import abspath, dirname, join
+from site import addsitedir
+
+sys.path.insert(0, abspath(join(dirname(__file__), "../../")))
+
+from django.conf import settings
+os.environ["DJANGO_SETTINGS_MODULE"] = "basic_project.settings"
+
+path = addsitedir(join(settings.PINAX_ROOT, "libs/external_libs"), set())
+if path:
+    sys.path = list(path) + sys.path
+
+sys.path.insert(0, join(settings.PINAX_ROOT, "apps/external_apps"))
+sys.path.insert(0, join(settings.PINAX_ROOT, "apps/local_apps"))
+sys.path.insert(0, join(settings.PROJECT_ROOT, "apps"))
+
+from django.core.servers.fastcgi import runfastcgi
+runfastcgi(method="threaded", daemonize="false")
---
+++ projects/temp_schedule_project/urls.py
@@ -0,0 +1,34 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+from django.views.generic.simple import direct_to_template
+
+from account.openid_consumer import PinaxConsumer
+
+from django.contrib import admin
+admin.autodiscover()
+
+import os
+
+urlpatterns = patterns('',
+    url(r'^$', direct_to_template, {"template": "homepage.html"}, name="home"),
+    
+    (r'^about/', include('about.urls')),
+    (r'^account/', include('account.urls')),
+    (r'^openid/(.*)', PinaxConsumer()),
+    (r'^profiles/', include('basic_profiles.urls')),
+    (r'^notices/', include('notification.urls')),
+    (r'^announcements/', include('announcements.urls')),
+    (r'^schedule/', include('schedule.urls')),
+    
+    (r'^admin/(.*)', admin.site.root),
+)
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': os.path.join(os.path.dirname(__file__), "site_media")}),
+    )
+    # Enable this once we moved all the media
+    # urlpatterns += patterns('',
+    #     (r'^site_media/(?P<path>.*)$', 'misc.views.serve'))
