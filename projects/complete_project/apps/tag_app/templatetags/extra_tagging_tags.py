ec6b32932ba0d5879e27a6e3c5a82458bc039499
James Tauber <jtauber@jtauber.com>
2009-02-01 08:34:23 +0000

    moving some common icons around and fixing links
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1474 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/complete_project/apps/tag_app/templatetags/extra_tagging_tags.py
+++ projects/complete_project/apps/tag_app/templatetags/extra_tagging_tags.py
@@ -1,10 +1,14 @@
 from django.template import Library
+from django.conf import settings
 
 register = Library()
 
 @register.inclusion_tag("tag_app/tag_list.html")
 def show_tags_for(obj):
-    return {"obj": obj}
+    return {
+        "obj": obj,
+        "MEDIA_URL": settings.MEDIA_URL,
+    }
 
 @register.inclusion_tag("tag_app/tag_count_list.html")
 def show_tag_counts(tag_counts):
