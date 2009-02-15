0f3fc779b79b8c7e18637fead2b88d23f1d9e856
James Tauber <jtauber@jtauber.com>
2009-02-15 04:30:46 +0000

    changed all url tabs to use named urls rather than view names
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1556 45601e1e-1555-4799-bd40-45c8c71eef50

--- apps/authsub/urls.py
+++ apps/authsub/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'authsub.views.login'),
+    url(r'^login/$', 'authsub.views.login', name="authsub_login"),
 )
--- apps/bbauth/urls.py
+++ apps/bbauth/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'bbauth.views.login'),
-    url(r'^success/$', 'bbauth.views.success'),
-    url(r'^logout/$', 'bbauth.views.logout'),
+    url(r'^login/$', 'bbauth.views.login', name="bbauth_login"),
+    url(r'^success/$', 'bbauth.views.success', name="bbauth_success"),
+    url(r'^logout/$', 'bbauth.views.logout', name="bbauth.logout"),
 )
