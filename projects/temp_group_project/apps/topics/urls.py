086b7923388ecd55097171f92b8cd71f750a23e7
James Tauber <jtauber@jtauber.com>
2009-02-17 08:25:00 +0000

    more steps towards separating topics from tribes (still broken)
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1583 45601e1e-1555-4799-bd40-45c8c71eef50

--- projects/temp_group_project/apps/topics/urls.py
+++ projects/temp_group_project/apps/topics/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^$', 'topics.views.topics', name="tribe_topics"),
-    url(r'^(\d+)/edit/$', 'topics.views.topic', kwargs={"edit": True}, name="tribe_topic_edit"),
-    url(r'^(\d+)/delete/$', 'topics.views.topic_delete', name="tribe_topic_delete"),
-    url(r'^(\d+)/$', 'topics.views.topic', name="tribe_topic"),
+    url(r'^$', 'topics.views.topics', name="topic_list"),
+    url(r'^(\d+)/edit/$', 'topics.views.topic', kwargs={"edit": True}, name="topic_edit"),
+    url(r'^(\d+)/delete/$', 'topics.views.topic_delete', name="topic_delete"),
+    url(r'^(\d+)/$', 'topics.views.topic', name="topic_detail"),
 )
