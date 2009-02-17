0687446fb56edfc02cb88015538f8752ea898725
James Tauber <jtauber@jtauber.com>
2009-02-17 05:11:23 +0000

    got autocomplete working in temp_group_project complete with temporary hacks
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1580 45601e1e-1555-4799-bd40-45c8c71eef50

--- apps/misc/views.py
+++ apps/misc/views.py
@@ -14,6 +14,8 @@ from django.utils.http import http_date
 from django.views.static import was_modified_since, directory_index
 from django.conf import settings
 
+
+
 SITE_MEDIA_ROOT = getattr(settings, 'MEDIA_ROOT',
     os.path.join(settings.PROJECT_ROOT, 'site_media'))
 PINAX_MEDIA_ROOT = os.path.join(settings.PINAX_ROOT, 'media', settings.PINAX_THEME)
@@ -109,9 +111,41 @@ def serve(request, path, show_indexes=False):
     return response
 
 
+# @@@ these can be cleaned up a lot, made more generic and with better queries
+
+def username_autocomplete_all(request):
+    if request.user.is_authenticated():
+        from django.contrib.auth.models import User
+        from basic_profiles.models import Profile
+        from avatar.templatetags.avatar_tags import avatar
+        q = request.GET.get("q")
+        users = User.objects.all()
+        content = []
+        # @@@ temporary hack -- don't try this at home (or on real sites)
+        for user in users:
+            if user.username.lower().startswith(q):
+                try:
+                    profile = user.get_profile()
+                    entry = "%s,,%s,,%s" % (
+                        avatar(user, 40),
+                        user.username,
+                        profile.location
+                    )
+                except Profile.DoesNotExist:
+                    pass
+                content.append(entry)
+        response = HttpResponse("\n".join(content))
+    else:
+        response = HttpResponseForbidden()
+    setattr(response, "djangologging.suppress_output", True)
+    return response
+
 
 def username_autocomplete_friends(request):
     if request.user.is_authenticated():
+        from friends.models import Friendship
+        from profiles.models import Profile
+        from avatar.templatetags.avatar_tags import avatar
         q = request.GET.get("q")
         friends = Friendship.objects.friends_for_user(request.user)
         content = []
