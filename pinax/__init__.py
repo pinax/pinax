57da55203a960a1bae00857aa1d8ed58672b908f
Brian Rosner <brosner@gmail.com>
2009-03-18 21:52:55 +0000

    Added a pre-release version item to VERSION tuple.
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1612 45601e1e-1555-4799-bd40-45c8c71eef50

--- pinax/__init__.py
+++ pinax/__init__.py
@@ -1,8 +1,8 @@
-VERSION = (0, 7, 0, "alpha")
+VERSION = (0, 7, 0, "alpha", 1)
 
 def get_version():
     if VERSION[3] != "final":
-        return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+        return "%s.%s.%s%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3], VERSION[4])
     else:
         return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
 
