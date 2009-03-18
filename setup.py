15ea3b544d99560b569aa9403c5c816885173d20
Jannis Leidel <jannis@leidel.info>
2009-03-18 21:21:46 +0000

    Added simple setup.py to package Pinax
    git-svn-id: http://svn.pinaxproject.com/pinax/trunk@1608 45601e1e-1555-4799-bd40-45c8c71eef50

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Pinax',
+    version=__import__('pinax').__version__,
+    description='Pinax is an open-source collection of re-usable apps for the Django Web Framework',
+    long_description=open('docs/intro.txt').read(),
+    author='James Tauber',
+    author_email='jtauber@jtauber.com',
+    maintainer='Jannis Leidel',
+    maintainer_email='jannis@leidel.info',
+    url='http://pinaxproject.com/',
+    packages=find_packages(),
+    include_package_data=True, # include package data under svn source control
+    setup_requires=['setuptools_git'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: JavaScript',
+    ],
+)
