45686fd94884954e7177ffc7826ea63ffd5c414a
Jannis Leidel <jannis@leidel.info>
2009-03-31 21:56:29 -0500

    Removed temp content project

--- pinax/projects/temp_content_project/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__about__ = """
-This project comes with a base set of apps that are good for building a content
-management system.  If you're looking to start a mainly content-driven web site,
-this is the project to choose.
-"""
