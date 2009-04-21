7d2ffa4ad70f4868d168ad8409037ff8b3174326
Jannis Leidel <jannis@leidel.info>
2009-04-21 15:02:46 +0200

    Split developer and release boot scripts.

--- scripts/create-venv-script.py
+++ scripts/create-venv-script.py
@@ -4,12 +4,36 @@ it will refresh the pinax-boot.py script
 """
 import os
 import virtualenv
-
-here = os.path.dirname(os.path.abspath(__file__))
-script_name = os.path.join(here, 'pinax-boot.py')
-extra_text = open(os.path.join(here,'_installer.py')).read()
+from optparse import OptionParser
+from os.path import join, exists, dirname, abspath
 
 def main():
+    usage = "usage: %prog [options]"
+    description = "Creates a Pinax boot script and uses version specific installer templates if given a release version."
+    parser = OptionParser(usage, description=description)
+    parser.add_option("-r", "--release",
+                      metavar='VERSION', dest='release', default=None,
+                      help='Release version of Pinax to bootstrap')
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose")
+    parser.add_option("-q", "--quiet",
+                      action="store_false", dest="verbose")
+    (options, args) = parser.parse_args()
+
+    here = dirname(abspath(__file__))
+    installer_template = join(here, 'installers', 'dev.py')
+    script_name = join(here, 'pinax-boot.py')
+
+    if options.release:
+        installer_template = join(here, 'installers', '%s.py' % options.release) # installers/0.7b1.py
+        if not exists(installer_template):
+            parser.error("template for release %s could not be found" % options.release)
+        script_name = join(here, 'pinax-boot-%s.py' % options.release) # pinax-boot-0.7b1.py
+
+    if options.verbose:
+        print "Using as template: %s" % installer_template
+
+    extra_text = open(installer_template).read()
     text = virtualenv.create_bootstrap_script(extra_text)
     if os.path.exists(script_name):
         f = open(script_name)
@@ -17,11 +41,15 @@ def main():
         f.close()
     else:
         cur_text = ''
-    print 'Updating %s' % script_name
+
+    if options.verbose:
+        print 'Updating %s' % script_name
+
     if cur_text == text:
         print 'No update'
     else:
-        print 'Script changed; updating...'
+        if options.verbose:
+            print 'Script changed; updating...'
         f = open(script_name, 'w')
         f.write(text)
         f.close()
