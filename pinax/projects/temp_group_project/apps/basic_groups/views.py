e4b700072770f1094d69172dc3069aff0b6e8ee2
James Tauber <jtauber@jtauber.com>
2009-05-11 14:14:31 +0100

    basic_groups view clean up and tests

--- pinax/projects/temp_group_project/apps/basic_groups/views.py
+++ pinax/projects/temp_group_project/apps/basic_groups/views.py
@@ -18,23 +18,19 @@ from basic_groups.forms import BasicGroupForm, BasicGroupUpdateForm
 
 @login_required
 def create(request, form_class=BasicGroupForm, template_name="basic_groups/create.html"):
-    if request.method == "POST":
-        if request.POST["action"] == "create": # @@@ why bother?
-            group_form = form_class(request.POST)
-            if group_form.is_valid():
-                group = group_form.save(commit=False)
-                group.creator = request.user
-                group.save()
-                group.members.add(request.user)
-                group.save()
-                if notification:
-                    # @@@ might be worth having a shortcut for sending to all users
-                    notification.send(User.objects.all(), "groups_new_group", {"group": group}, queue=True)
-                return HttpResponseRedirect(group.get_absolute_url())
-        else:
-            group_form = form_class()
-    else:
-        group_form = form_class()
+    
+    group_form = form_class(request.POST or None)
+    
+    if group_form.is_valid():
+        group = group_form.save(commit=False)
+        group.creator = request.user
+        group.save()
+        group.members.add(request.user)
+        group.save()
+        if notification:
+            # @@@ might be worth having a shortcut for sending to all users
+            notification.send(User.objects.all(), "groups_new_group", {"group": group}, queue=True)
+        return HttpResponseRedirect(group.get_absolute_url())
     
     return render_to_response(template_name, {
         "group_form": group_form,
@@ -74,31 +70,31 @@ def your_groups(request, template_name="basic_groups/your_groups.html"):
 def group(request, group_slug=None, form_class=BasicGroupUpdateForm, template_name="basic_groups/group.html"):
     group = get_object_or_404(BasicGroup, slug=group_slug)
     
-    if request.user.is_authenticated() and request.method == "POST":
-        if request.POST["action"] == "update" and request.user == group.creator:
-            group_form = form_class(request.POST, instance=group)
-            if group_form.is_valid():
-                group = group_form.save()
-        else:
-            group_form = form_class(instance=group)
-        if request.POST["action"] == "join":
-            group.members.add(request.user)
-            request.user.message_set.create(message="You have joined the group %s" % group.name)
-            if notification:
-                notification.send([group.creator], "groups_created_new_member", {"user": request.user, "group": group})
-                notification.send(group.members.all(), "groups_new_member", {"user": request.user, "group": group})
-        elif request.POST["action"] == "leave":
-            group.members.remove(request.user)
-            request.user.message_set.create(message="You have left the group %s" % group.name)
-            if notification:
-                pass # @@@ no notification on departure yet
-    else:
-        group_form = form_class(instance=group)
+    group_form = form_class(request.POST or None, instance=group)
     
-    are_member = request.user in group.members.all()
+    action = request.POST.get('action')
+    
+    if action == "update" and group_form.is_valid():
+        group = group_form.save()
+    elif action == "join":
+        group.members.add(request.user)
+        request.user.message_set.create(message="You have joined the group %s" % group.name)
+        if notification:
+            notification.send([group.creator], "groups_created_new_member", {"user": request.user, "group": group})
+            notification.send(group.members.all(), "groups_new_member", {"user": request.user, "group": group})
+    elif action == "leave":
+        group.members.remove(request.user)
+        request.user.message_set.create(message="You have left the group %s" % group.name)
+        if notification:
+            pass # @@@ no notification on departure yet
+    
+    if not request.user.is_authenticated():
+        is_member = False
+    else:
+        is_member = group.user_is_member(request.user)
     
     return render_to_response(template_name, {
         "group_form": group_form,
         "group": group,
-        "are_member": are_member,
+        "is_member": is_member,
     }, context_instance=RequestContext(request))
