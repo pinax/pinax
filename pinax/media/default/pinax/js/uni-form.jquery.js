8cb5ba013d71e307878e06110a4550908bfc14c5
Daniel Greenfeld <pydanny@gmail.com>
2009-05-17 15:22:45 -0400

    For #247 this updates the Pinax uni-form js with the django-uni-form js that is in 0.4.1b

--- pinax/media/default/pinax/js/uni-form.jquery.js
+++ pinax/media/default/pinax/js/uni-form.jquery.js
@@ -9,26 +9,11 @@ jQuery.fn.uniform = function(settings) {
   
   return this.each(function() {
     var form = jQuery(this);
-    
-    // Focus specific control holder
-    var focusControlHolder = function(element) {
-      var parent = element.parent();
-      
-      while(typeof(parent) == 'object') {
-        if(parent) {
-          if(parent[0] && (parent[0].className.indexOf(settings.holder_class) >= 0)) {
-            parent.addClass(settings.focused_class);
-            return;
-          } // if
-        } // if
-        parent = jQuery(parent.parent());
-      } // while
-    };
-    
+ 
     // Select form fields and attach them higlighter functionality
     form.find(settings.field_selector).focus(function() {
       form.find('.' + settings.focused_class).removeClass(settings.focused_class);
-      focusControlHolder(jQuery(this));
+      jQuery(this).parents("." + settings.holder_class).addClass(settings.focused_class)
     }).blur(function() {
       form.find('.' + settings.focused_class).removeClass(settings.focused_class);
     });
@@ -38,4 +23,5 @@ jQuery.fn.uniform = function(settings) {
 // Auto set on page load...
 $(document).ready(function() {
   jQuery('form.uniForm').uniform();
+  $(document.activeElement).focus() //safari doesn't support this and has no alternative
 });
