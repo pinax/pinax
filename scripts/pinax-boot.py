313bcde5cc16a0472a32b3f7f61ebe457f4c92cb
Jannis Leidel <jannis@leidel.info>
2009-05-27 22:03:16 +0200

    Added pip-0.4 tarball and updated boot strap script to use it

--- scripts/pinax-boot.py
+++ scripts/pinax-boot.py
@@ -974,7 +974,7 @@ PINAX_PYPI = 'http://pypi.pinaxproject.com'
 PINAX_MUST_HAVES = {
     'setuptools-git': 'setuptools_git-0.3.3.tar.gz',
     'setuptools-dummy': 'setuptools_dummy-0.0.3.tar.gz',
-    'pip': 'pip-0.3.1.tar.gz',
+    'pip': 'pip-0.4.tar.gz',
 }
 
 if sys.platform == 'win32':
