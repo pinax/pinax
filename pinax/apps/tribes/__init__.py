488d142d7ab63bdf2f0298bb601546b9e5520a43
Brian Rosner <brosner@gmail.com>
2009-06-21 22:34:43 -0600

    Corrected some text in projects and tribe exceptions.

--- pinax/apps/tribes/__init__.py
+++ pinax/apps/tribes/__init__.py
@@ -1,3 +1,3 @@
 raise Exception("If you see this you have found a place where tribes app is "
-    "imported. We are currently trying to move to a new tribe app. Please "
+    "imported. We are currently trying to move to a new tribes app. Please "
     "report this at http://code.pinaxproject.com/")
