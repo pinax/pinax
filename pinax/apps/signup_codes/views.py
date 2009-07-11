d58ca59f109c63364ad14d852d3cd9a727c75bc7
Brian Rosner <brosner@gmail.com>
2009-07-10 23:40:46 -0600

    Show a successful message once admin invite e-mail has been sent

--- pinax/apps/signup_codes/views.py
+++ pinax/apps/signup_codes/views.py
@@ -60,7 +60,9 @@ def admin_invite_user(request, form_class=InviteUserForm,
     if request.method == "POST":
         form = form_class(request.POST)
         if form.is_valid():
+            email = form.cleaned_data["email"]
             form.send_signup_code()
+            request.user.message_set.create(message=ugettext("An e-mail has been sent to %(email)s.") % {"email": email})
             form = form_class() # reset
     else:
         form = form_class()
