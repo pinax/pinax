1142285a50e5bea9b84034171861fb6d3c451e8a
Brian Rosner <brosner@gmail.com>
2009-07-10 19:32:46 -0600

    Added {% else %} branching support in {% ifsetting %} templatetag

--- pinax/apps/misc/templatetags/ifsetting_tag.py
+++ pinax/apps/misc/templatetags/ifsetting_tag.py
@@ -5,8 +5,9 @@ from django.conf import settings
 register = template.Library()
 
 class IfSettingNode(template.Node):
-    def __init__(self, nodelist, setting):
-        self.nodelist = nodelist
+    def __init__(self, nodelist_true, nodelist_false, setting):
+        self.nodelist_true = nodelist_true
+        self.nodelist_false = nodelist_false
         self.setting = setting
         
     def render(self, context):
@@ -16,13 +17,20 @@ class IfSettingNode(template.Node):
             return ""
         else:
             if setting_value:
-                return self.nodelist.render(context)
+                return self.nodelist_true.render(context)
+            else:
+                return self.nodelist_false.render(context)
             return ""
 
 @register.tag
 def ifsetting(parser, token):
     bits = token.split_contents()
-    nodelist = parser.parse(("endifsetting"))
-    parser.delete_first_token()
-    return IfSettingNode(nodelist, bits[1])
+    nodelist_true = parser.parse(("else", "endifsetting"))
+    token = parser.next_token()
+    if token.contents == "else":
+        nodelist_false = parser.parse(("endifsetting",))
+        parser.delete_first_token()
+    else:
+        nodelist_false = template.NodeList()
+    return IfSettingNode(nodelist_true, nodelist_false, bits[1])
     
