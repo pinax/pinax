23f73cb045c04fcea6a74e1aec31bd8f91413cd0
Brian Rosner <brosner@gmail.com>
2009-07-24 14:50:10 -0600

    Fixed #307 — Cleaned up tag_app of vestigial code related to projects and fixed tribe related bits
    Thanks skyl for the report.

--- pinax/projects/social_project/apps/tag_app/views.py
+++ pinax/projects/social_project/apps/tag_app/views.py
@@ -5,13 +5,11 @@ from blog.models import Post
 from tagging.models import Tag, TaggedItem
 from photos.models import Image
 from bookmarks.models import BookmarkInstance
-#from projects.models import Project, Task
-#from projects.models import Topic as ProjectTopic
-
-#from tribes.models import Tribe
-#from tribes.models import Topic as TribeTopic
+# from tribes.models import Tribe
+# from tribes.models import Topic as TribeTopic
 
 from wiki.models import Article as WikiArticle
+
 
 def tags(request, tag, template_name='tags/index.html'):
     tag = get_object_or_404(Tag, name=tag)
@@ -21,12 +19,8 @@ def tags(request, tag, template_name='tags/index.html'):
     phototags = TaggedItem.objects.get_by_model(Image, tag)
     bookmarktags = TaggedItem.objects.get_by_model(BookmarkInstance, tag)
     
-    #project_tags = TaggedItem.objects.get_by_model(Project, tag).filter(deleted=False)
-    #project_topic_tags = TaggedItem.objects.get_by_model(ProjectTopic, tag).filter(project__deleted=False)
-    #project_task_tags = TaggedItem.objects.get_by_model(Task, tag).filter(project__deleted=False)
-    
-    #tribe_tags = TaggedItem.objects.get_by_model(Tribe, tag).filter(deleted=False)
-    #tribe_topic_tags = TaggedItem.objects.get_by_model(TribeTopic, tag).filter(tribe__deleted=False)
+    # tribe_tags = TaggedItem.objects.get_by_model(Tribe, tag).filter(deleted=False)
+    # tribe_topic_tags = TaggedItem.objects.get_by_model(TribeTopic, tag).filter(tribe__deleted=False)
     
     # @@@ TODO: tribe_wiki_article_tags and project_wiki_article_tags
     wiki_article_tags = TaggedItem.objects.get_by_model(WikiArticle, tag)
@@ -36,10 +30,7 @@ def tags(request, tag, template_name='tags/index.html'):
         'alltags': alltags,
         'phototags': phototags,
         'bookmarktags': bookmarktags,
-        'project_tags': project_tags,
-        #'project_topic_tags': project_topic_tags,
-        #'project_task_tags': project_task_tags,
-        #'tribe_tags': tribe_tags,
-        #'tribe_topic_tags': tribe_topic_tags,
+        # 'tribe_tags': tribe_tags,
+        # 'tribe_topic_tags': tribe_topic_tags,
         'wiki_article_tags': wiki_article_tags,
     }, context_instance=RequestContext(request))
