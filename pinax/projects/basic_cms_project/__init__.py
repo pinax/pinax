b1434be153e036d9a0c2b45840d818e4f39db653
James Tauber <jtauber@jtauber.com>
2009-07-24 23:54:42 -0400

    fixed about message for basic_cms_project

--- pinax/projects/basic_cms_project/__init__.py
+++ pinax/projects/basic_cms_project/__init__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 __about__ = """
-This project comes with the bare minimum set of applications and templates to
-get you started. It includes no extra tabs–only the profile and notices tabs are
-included by default. From here you can add any extra functionality and
-applications that you would like.
+A very simple CMS that lets you set up templates and then edit content,
+including images, right in the frontend of the site.
 """
