55f3c96320eec90984863c7b01e21cdf1596393a
Jannis Leidel <jannis@leidel.info>
2009-07-25 02:36:07 +0200

    Stopped inserting svn revisions of apps in use in request dictionary

--- pinax/apps/misc/__init__.py
+++ pinax/apps/misc/__init__.py
@@ -20,25 +20,4 @@ def clean_request_for_except_repr(signal=None, sender=None, request=None, **kwar
         request.POST._mutable = mutable
     return masked
 
-def insert_svn_app_versions(signal=None, sender=None, request=None, **kwargs):
-    if not request or not request.META: return False
-    try:
-        from templatetags.svn_app_version import get_all_versions
-    except:
-        return False
-    mutable = True
-    if hasattr(request.META, '_mutable'):
-        mutable = request.META._mutable
-        request.META._mutable = True
-    added = False
-    prefix = 'svn_app_version'
-    for app, version in get_all_versions(True):
-        request.META[prefix + app] = version
-        prefix = 'svn_app_version.'
-        added=True
-    if hasattr(request.META, '_mutable'):
-        request.META._mutable = mutable
-    return added
-
 signals.got_request_exception.connect(clean_request_for_except_repr)
-signals.got_request_exception.connect(insert_svn_app_versions)
