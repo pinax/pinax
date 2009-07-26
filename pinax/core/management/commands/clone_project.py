pinax/pinax

commit 01bc980e7b70e967e9f19fea2286666ba71c15be

Fixed #323 — Print help when no args are given to clone_project. Thanks mikl for the report.

pinax/core/management/commands/clone_project.py
@@ -182,6 +182,11 @@ class Command(BaseCommand):
         operations.
         """
         
+        if not args:
+            # note: --help prints full path to pinax-admin
+            self.print_help("pinax-admin", "clone_project")
+            sys.exit(0)
+        
         if options.get('list_projects'):
             pinax_root = get_pinax_root(options.get('pinax_root'))
             print "Available Projects"

commit 90f3e3dcb69ee63383709226a5a4bf6d4f1fc74d

Fixed #330 — Test for options before assuming no arguments were passed in.
This restores proper -l/--list-projects behavior broken by 01bc980

Signed-off-by: Brian Rosner <brosner@gmail.com>

pinax/core/management/commands/clone_project.py
@@ -182,11 +182,6 @@ class Command(BaseCommand):
         operations.
         """
         
-        if not args:
-            # note: --help prints full path to pinax-admin
-            self.print_help("pinax-admin", "clone_project")
-            sys.exit(0)
-        
         if options.get('list_projects'):
             pinax_root = get_pinax_root(options.get('pinax_root'))
             print "Available Projects"
@@ -198,6 +193,11 @@ class Command(BaseCommand):
                 for line in about.strip().splitlines():
                     print '    %s' % line
                 print
+            sys.exit(0)
+        
+        if not args:
+            # note: --help prints full path to pinax-admin
+            self.print_help("pinax-admin", "clone_project")
             sys.exit(0)
         
         main(options.get('pinax_root'), args[0], args[1],