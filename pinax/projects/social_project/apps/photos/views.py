24bc6487ee1892a7b57ae85cc740365e5f3e0135
Brian Rosner <brosner@gmail.com>
2009-07-26 04:56:17 -0400

    Added initial pass for photos group support and various clean-ups

--- pinax/projects/social_project/apps/photos/views.py
+++ pinax/projects/social_project/apps/photos/views.py
@@ -4,20 +4,31 @@ from django.template import RequestContext
 from django.db.models import Q
 from django.http import Http404
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ObjectDoesNotExist
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from photologue.models import *
-from photos.models import Image
+from photos.models import Image, Pool
 from photos.forms import PhotoUploadForm, PhotoEditForm
 
 
+@login_required
 def upload(request, form_class=PhotoUploadForm,
-        template_name="photos/upload.html"):
+        template_name="photos/upload.html", group_slug=None, bridge=None):
     """
     upload form for photos
     """
+    
+    if bridge:
+        try:
+            group = bridge.get_group(group_slug)
+        except ObjectDoesNotExist:
+            raise Http404
+    else:
+        group = None
+    
     photo_form = form_class()
     if request.method == 'POST':
         if request.POST.get("action") == "upload":
@@ -26,41 +37,94 @@ def upload(request, form_class=PhotoUploadForm,
                 photo = photo_form.save(commit=False)
                 photo.member = request.user
                 photo.save()
+                
+                # in group context we create a Pool object for it
+                if group:
+                    pool = Pool()
+                    pool.photo = photo
+                    group.associate(pool)
+                    pool.save()
+                
                 request.user.message_set.create(message=_("Successfully uploaded photo '%s'") % photo.title)
-                return HttpResponseRedirect(reverse('photo_details', args=(photo.id,)))
+                
+                include_kwargs = {"id": photo.id}
+                if group:
+                    redirect_to = bridge.reverse("photo_details", group, kwargs=include_kwargs)
+                else:
+                    redirect_to = reverse("photo_details", kwargs=include_kwargs)
+                
+                return HttpResponseRedirect(redirect_to)
 
     return render_to_response(template_name, {
+        "group": group,
         "photo_form": photo_form,
     }, context_instance=RequestContext(request))
-upload = login_required(upload)
 
-def yourphotos(request, template_name="photos/yourphotos.html"):
+
+@login_required
+def yourphotos(request, template_name="photos/yourphotos.html", group_slug=None, bridge=None):
     """
     photos for the currently authenticated user
     """
+    
+    if bridge:
+        try:
+            group = bridge.get_group(group_slug)
+        except ObjectDoesNotExist:
+            raise Http404
+    else:
+        group = None
+    
+    # @@@ group support
     photos = Image.objects.filter(member=request.user).order_by("-date_added")
+    
     return render_to_response(template_name, {
+        "group": group,
         "photos": photos,
     }, context_instance=RequestContext(request))
-yourphotos = login_required(yourphotos)
 
-def photos(request, template_name="photos/latest.html"):
+
+@login_required
+def photos(request, template_name="photos/latest.html", group_slug=None, bridge=None):
     """
     latest photos
     """
+    
+    if bridge:
+        try:
+            group = bridge.get_group(group_slug)
+        except ObjectDoesNotExist:
+            raise Http404
+    else:
+        group = None
+    
+    # @@@ group support
     photos = Image.objects.filter(
         Q(is_public=True) |
         Q(is_public=False, member=request.user)
     ).order_by("-date_added")
+    
     return render_to_response(template_name, {
+        "group": group,
         "photos": photos,
     }, context_instance=RequestContext(request))
-photos = login_required(photos)
 
-def details(request, id, template_name="photos/details.html"):
+
+@login_required
+def details(request, id, template_name="photos/details.html", group_slug=None, bridge=None):
     """
     show the photo details
     """
+    
+    if bridge:
+        try:
+            group = bridge.get_group(group_slug)
+        except ObjectDoesNotExist:
+            raise Http404
+    else:
+        group = None
+    
+    # @@@ group support
     photo = get_object_or_404(Image, id=id)
     # @@@: test
     if not photo.is_public and request.user != photo.member:
@@ -69,47 +133,89 @@ def details(request, id, template_name="photos/details.html"):
     
     title = photo.title
     host = "http://%s" % get_host(request)
+    
     if photo.member == request.user:
         is_me = True
     else:
         is_me = False
     
     return render_to_response(template_name, {
+        "group": group,
         "host": host, 
         "photo": photo,
         "photo_url": photo_url,
         "is_me": is_me,
     }, context_instance=RequestContext(request))
-details = login_required(details)
 
-def memberphotos(request, username, template_name="photos/memberphotos.html"):
+
+@login_required
+def memberphotos(request, username, template_name="photos/memberphotos.html", group_slug=None, bridge=None):
     """
     Get the members photos and display them
     """
+    
+    if bridge:
+        try:
+            group = bridge.get_group(group_slug)
+        except ObjectDoesNotExist:
+            raise Http404
+    else:
+        group = None
+    
     user = get_object_or_404(User, username=username)
-    photos = Image.objects.filter(member__username=username, is_public=True).order_by("-date_added")
+    photos = Image.objects.filter(
+        member__username = username,
+        is_public = True,
+    ).order_by("-date_added")
+    
     return render_to_response(template_name, {
+        "group": group,
         "photos": photos,
     }, context_instance=RequestContext(request))
-memberphotos = login_required(memberphotos)
 
+
+@login_required
 def edit(request, id, form_class=PhotoEditForm,
-        template_name="photos/edit.html"):
+        template_name="photos/edit.html", group_slug=None, bridge=None):
+    
+    if bridge:
+        try:
+            group = bridge.get_group(group_slug)
+        except ObjectDoesNotExist:
+            raise Http404
+    else:
+        group = None
+    
+    # @@@ group support
     photo = get_object_or_404(Image, id=id)
     photo_url = photo.get_display_url()
 
     if request.method == "POST":
         if photo.member != request.user:
             request.user.message_set.create(message="You can't edit photos that aren't yours")
+            
+            include_kwargs = {"id": photo.id}
+            if group:
+                redirect_to = bridge.reverse("photo_details", group, kwargs=include_kwargs)
+            else:
+                redirect_to = reverse("photo_details", kwargs=include_kwargs)
+            
             return HttpResponseRedirect(reverse('photo_details', args=(photo.id,)))
         if request.POST["action"] == "update":
             photo_form = form_class(request.user, request.POST, instance=photo)
             if photo_form.is_valid():
                 photoobj = photo_form.save(commit=False)
                 photoobj.save()
+                
                 request.user.message_set.create(message=_("Successfully updated photo '%s'") % photo.title)
-                                
-                return HttpResponseRedirect(reverse('photo_details', args=(photo.id,)))
+                
+                include_kwargs = {"id": photo.id}
+                if group:
+                    redirect_to = bridge.reverse("photo_details", group, kwargs=include_kwargs)
+                else:
+                    redirect_to = reverse("photo_details", kwargs=include_kwargs)
+                
+                return HttpResponseRedirect(redirect_to)
         else:
             photo_form = form_class(instance=photo)
 
@@ -117,21 +223,38 @@ def edit(request, id, form_class=PhotoEditForm,
         photo_form = form_class(instance=photo)
 
     return render_to_response(template_name, {
+        "group": group,
         "photo_form": photo_form,
         "photo": photo,
         "photo_url": photo_url,
     }, context_instance=RequestContext(request))
-edit = login_required(edit)
 
+@login_required
 def destroy(request, id):
+    
+    if bridge:
+        try:
+            group = bridge.get_group(group_slug)
+        except ObjectDoesNotExist:
+            raise Http404
+    else:
+        group = None
+    
+    # @@ group support
     photo = Image.objects.get(pk=id)
     title = photo.title
+    
+    if group:
+        redirect_to = bridge.reverse("photos_yours", group)
+    else:
+        redirect_to = reverse("photos_yours")
+    
     if photo.member != request.user:
         request.user.message_set.create(message="You can't delete photos that aren't yours")
-        return HttpResponseRedirect(reverse("photos_yours"))
+        return HttpResponseRedirect(redirect_to)
 
     if request.method == "POST" and request.POST["action"] == "delete":
         photo.delete()
         request.user.message_set.create(message=_("Successfully deleted photo '%s'") % title)
-    return HttpResponseRedirect(reverse("photos_yours"))
-destroy = login_required(destroy)
+    
+    return HttpResponseRedirect(redirect_to)
