29d22bff24eda5827e9879b307b6cb4f22aab209
Jannis Leidel <jannis@leidel.info>
2009-07-26 16:51:47 +0200

    Added optional sort order to voting templatetags.

--- pinax/apps/voting_extras/templatetags/extra_voting_tags.py
+++ pinax/apps/voting_extras/templatetags/extra_voting_tags.py
@@ -11,20 +11,24 @@ register = template.Library()
 
 def do_order_by_votes(parser, token):
     split = token.split_contents()
-    if len(split) == 2:
-        return OrderByVotesNode(split[1])
+    if len(split) in (2, 3):
+        return OrderByVotesNode(*split[1:])
     else:
-        raise template.TemplateSyntaxError('%r tag takes one argument.' % split[0])
+        raise template.TemplateSyntaxError('%r tag takes one or two arguments.' % split[0])
 
 class OrderByVotesNode(template.Node):
-    def __init__(self, queryset_var):
+    def __init__(self, queryset_var, direction='desc'):
         self.queryset_var = template.Variable(queryset_var)
+        self.direction = direction
     
     def render(self, context):
         key = self.queryset_var.var
         value = self.queryset_var.resolve(context)
+        try:
+            direction = template.Variable(self.direction).resolve(context)
+        except template.VariableDoesNotExist:
+            direction = 'desc'
         model = value.model
-        
         qn = connection.ops.quote_name
         ctype = ContentType.objects.get_for_model(model)
         by_score = model.objects.extra(select={'score': """
@@ -33,22 +37,23 @@ class OrderByVotesNode(template.Node):
                 WHERE content_type_id = %s
                 AND object_id = %s.%s
             """ % (qn(Vote._meta.db_table), ctype.id, qn(model._meta.db_table), qn(model._meta.pk.attname))},
-            order_by=['-score'])
+            order_by=[(direction == 'desc' and '-' or '') + 'score'])
         context[key] = by_score
         return u""
 
 def do_order_by_reddit(parser, token):
     split = token.split_contents()
-    if len(split) == 3:
+    if len(split) in (3, 4):
         return OrderByRedditNode(*split[1:])
     else:
-        raise template.TemplateSyntaxError('%r tag takes one argument.' % split[0])
+        raise template.TemplateSyntaxError('%r tag takes one or two arguments.' % split[0])
 
 class OrderByRedditNode(template.Node):
-    def __init__(self, queryset_var, date_var):
+    def __init__(self, queryset_var, date_var, direction='desc'):
         self.queryset_var = template.Variable(queryset_var)
         self.date_var = date_var
-    
+        self.direction = direction
+
     def render(self, context):
         key = self.queryset_var.var
         values = self.queryset_var.resolve(context)
@@ -73,8 +78,12 @@ class OrderByRedditNode(template.Node):
             ratings.append((rating, obj))
         ratings.sort(cmp=lambda x, y: cmp(y[0], x[0]))
         by_score = [x[1] for x in ratings]
-        
-        
+        try:
+            direction = template.Variable(self.direction).resolve(context)
+        except template.VariableDoesNotExist:
+            direction = 'desc'
+        if direction == 'asc':
+            by_score.reverse()
         context[key] = by_score
         return u""
 
