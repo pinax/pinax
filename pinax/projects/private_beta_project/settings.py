9e2a1d38dd5d47da7f44a0bbc63052366156ad81
Brian Rosner <brosner@gmail.com>
2009-08-11 21:25:23 -0600

    Allow e-mail confirmation through AuthenticatedMiddleware in private_beta_project

--- pinax/projects/private_beta_project/settings.py
+++ pinax/projects/private_beta_project/settings.py
@@ -170,6 +170,7 @@ AUTHENTICATED_EXEMPT_URLS = [
     r"^/success/$",
     r"^/account/signup/$",
     r"^/account/password_reset/",
+    r"^/account/confirm_email",
 ]
 
 # local_settings.py can be used to override environment-specific settings
