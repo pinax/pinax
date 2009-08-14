fe1f1c6dbac02b840e4176f850fd9cd99bd9ff65
Brian Rosner <brosner@gmail.com>
2009-08-14 03:40:42 -0600

    Persist state filtering when grouping by

--- pinax/apps/tasks/views.py
+++ pinax/apps/tasks/views.py
@@ -84,9 +84,14 @@ def tasks(request, group_slug=None, template_name="tasks/task_list.html", bridge
     
     task_filter = TaskFilter(filter_data, queryset=tasks)
     
+    group_by_querydict = request.GET.copy()
+    group_by_querydict.pop("group_by", None)
+    group_by_querystring = group_by_querydict.urlencode()
+    
     return render_to_response(template_name, {
         "group": group,
         "group_by": group_by,
+        "gbqs": group_by_querystring,
         "is_member": is_member,
         "group_base": group_base,
         "task_filter": task_filter,
@@ -493,6 +498,10 @@ def focus(request, field, value, group_slug=None, template_name="tasks/focus.htm
         # Django will not merge queries that are both not distinct or distinct
         tasks = tasks.distinct() & task_filter.qs
     
+    group_by_querydict = request.GET.copy()
+    group_by_querydict.pop("group_by", None)
+    group_by_querystring = group_by_querydict.urlencode()
+    
     return render_to_response(template_name, {
         "group": group,
         "task_filter": task_filter,
@@ -500,6 +509,7 @@ def focus(request, field, value, group_slug=None, template_name="tasks/focus.htm
         "field": field,
         "value": value,
         "group_by": group_by,
+        "gbqs": group_by_querystring,
         "is_member": is_member,
         "group_base": group_base,
     }, context_instance=RequestContext(request))
