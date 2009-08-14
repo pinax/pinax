bb2d742c392ce96884c57a70cc33a8fd54a2e819
Brian Rosner <brosner@gmail.com>
2009-08-13 22:44:09 -0600

    Use forms.CheckboxSelectMultiple to select task state

--- pinax/apps/tasks/filters.py
+++ pinax/apps/tasks/filters.py
@@ -1,10 +1,16 @@
+from django import forms
+
 import filter as filters
 
 from tasks.models import Task
 
+
 class TaskFilter(filters.FilterSet):
     
-    state = filters.MultipleChoiceFilter(choices=Task.STATE_CHOICES)
+    state = filters.MultipleChoiceFilter(
+        choices = Task.STATE_CHOICES,
+        widget = forms.CheckboxSelectMultiple,
+    )
     
     class Meta:
         model = Task
