3ca5c0cf373b54894a07bc737af141fe061b6c72
Brian Rosner <brosner@gmail.com>
2009-08-22 01:20:13 -0600

    Minor clean up in photos/templatetags/photo_tags.py

--- pinax/apps/photos/templatetags/photo_tags.py
+++ pinax/apps/photos/templatetags/photo_tags.py
@@ -1,23 +1,14 @@
 import re
+
 from django import template
-#from photologue.utils import EXIF
+
+
 register = template.Library()
 
-@register.tag(name="print_exif")
-def do_print_exif(parser, token):
-    try:
-        tag_name, exif = token.contents.split()
-    except ValueError:
-        msg = '%r tag requires a single argument' % token.contents[0]
-        raise template.TemplateSyntaxError(msg)
-
-    exif = parser.compile_filter(exif)
-    return PrintExifNode(exif)
 
 class PrintExifNode(template.Node):
 
     def __init__(self, exif):
-
         self.exif = exif
 
     def render(self, context):
@@ -36,3 +27,14 @@ class PrintExifNode(template.Node):
 
         return u'<div id="exif">%s</div>' % (msg)
 
+
+@register.tag(name="print_exif")
+def do_print_exif(parser, token):
+    try:
+        tag_name, exif = token.contents.split()
+    except ValueError:
+        msg = '%r tag requires a single argument' % token.contents[0]
+        raise template.TemplateSyntaxError(msg)
+
+    exif = parser.compile_filter(exif)
+    return PrintExifNode(exif)
