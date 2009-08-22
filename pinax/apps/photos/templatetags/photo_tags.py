bd204dc680af979ed0677afb567b3b9b0d6529e0
Brian Rosner <brosner@gmail.com>
2009-08-22 01:45:51 -0600

    Fixed profile page from showing private photos to other users

--- pinax/apps/photos/templatetags/photo_tags.py
+++ pinax/apps/photos/templatetags/photo_tags.py
@@ -2,6 +2,8 @@ import re
 
 from django import template
 
+from photos.models import Image
+
 
 register = template.Library()
 
@@ -38,3 +40,38 @@ def do_print_exif(parser, token):
 
     exif = parser.compile_filter(exif)
     return PrintExifNode(exif)
+
+
+class PublicPhotosNode(template.Node):
+    
+    def __init__(self, user_var, context_var):
+        self.user_var = template.Variable(user_var)
+        self.context_var = context_var
+    
+    def render(self, context):
+        user = self.user_var.resolve(context)
+        
+        photos = Image.objects.filter(member=user, is_public=True)
+        
+        context[self.context_var] = photos
+        return ""
+
+@register.tag
+def public_photos(parser, token):
+    # @@@ this tag can be improved to do more like public photos globally or
+    # in a group
+    
+    bits = token.split_contents()
+    
+    if len(bits) != 5:
+        message = "'%s' tag requires five arguments" % bits[0]
+        raise template.TemplateSyntaxError(message)
+    else:
+        if bits[1] != 'for':
+            message = "'%s' second argument must be 'for'" % bits[0]
+            raise template.TemplateSyntaxError(message)
+        if bits[3] != 'as':
+            message = "'%s' forth argument must be 'as'" % bits[0]
+            raise template.TemplateSyntaxError(message)
+    
+    return PublicPhotosNode(bits[2], bits[4])
