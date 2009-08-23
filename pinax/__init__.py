9937355756ec8d11c4466e856dc08f41758c1e71
Brian Rosner <brosner@gmail.com>
2009-08-23 02:07:29 -0600

    When on final release don't include the word final in version string

--- pinax/__init__.py
+++ pinax/__init__.py
@@ -4,10 +4,8 @@ def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
     if VERSION[2]:
         version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3]:
-        version = '%s%s' % (version, VERSION[3])
-        if VERSION[3] != 'final':
-            version = '%s%s' % (version, VERSION[4])
+    if VERSION[3] != "final":
+        version = '%s%s%s' % (version, VERSION[3], VERSION[4])
     return version
 
 __version__ = get_version()
