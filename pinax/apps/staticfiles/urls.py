69a4586503dc09e772da95907b537621911bc252
Jannis Leidel <jannis@leidel.info>
2009-09-02 13:45:54 +0200

    Removed staticfiles app after releasing it on PyPI

--- pinax/apps/staticfiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from django.conf import settings
-
-urlpatterns = patterns('', 
-    (r'^static/(?P<path>.*)$', 'staticfiles.views.serve'),
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT
-    }),
-)
