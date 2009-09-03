694fa132af6b3f973b0e6c482f61785eba89fb36
Jannis Leidel <jannis@leidel.info>
2009-09-03 05:04:09 +0200

    Added verbose_name and verbose_name_plural to waiting list entry model

--- pinax/apps/waitinglist/models.py
+++ pinax/apps/waitinglist/models.py
@@ -1,9 +1,13 @@
-
 from datetime import datetime
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class WaitingListEntry(models.Model):
     email = models.EmailField(unique=True)
     created = models.DateTimeField(default=datetime.now, editable=False)
+
+    class Meta:
+        verbose_name = _('waiting list entry')
+        verbose_name_plural = _('waiting list entries')
