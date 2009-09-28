b670da78c1d37ee961fe22ec5b660ddf446cc3f2
Jakob Torp Svendsen <jakob@revealit.dk>
2009-09-28 11:53:41 +0200

    Fixed #487 Missing notice type
    Signed-off-by: Jannis Leidel <jannis@leidel.info>

--- pinax/apps/projects/management.py
+++ pinax/apps/projects/management.py
@@ -9,6 +9,7 @@ if "notification" in settings.INSTALLED_APPS:
         notification.create_notice_type("projects_new_member", _("New Project Member"), _("a project you are a member of has a new member"), default=1)
         notification.create_notice_type("projects_created_new_member", _("New Member Of Project You Created"), _("a project you created has a new member"), default=2)
         notification.create_notice_type("projects_new_project", _("New Project Created"), _("a new project has been created"), default=1)
+        notification.create_notice_type("projects_added_as_member", _("Added as a Project Member"), _("you have become member of a project"), default=1)
         
     signals.post_syncdb.connect(create_notice_types, sender=notification)
 else:
