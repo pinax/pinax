ddfd4b8cac714ffc30c73604e5399860210054f0
Brian Rosner <brosner@gmail.com>
2010-01-02 23:08:06 -0700

    Updated DATABASE_ENGINE comment to reflect proper 1.0 state (removed ado_mssql and added oracle)

--- pinax/projects/company_project/settings.py
+++ pinax/projects/company_project/settings.py
@@ -22,7 +22,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'dev.db'       # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
