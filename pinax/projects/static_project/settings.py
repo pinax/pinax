49f3190f00dd32cb5c4724538ed4e60200bf5fa9
Brian Rosner <brosner@gmail.com>
2010-01-02 23:05:36 -0700

    Added missing DATABASE_* settings in static_project

--- pinax/projects/static_project/settings.py
+++ pinax/projects/static_project/settings.py
@@ -18,6 +18,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = ''             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
