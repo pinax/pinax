85c0ca962607d694a1ee68c2bbab7b2ac41a0b1d
Brian Rosner <brosner@gmail.com>
2010-01-16 16:52:39 -0700

    Enabled CSRF protection from Django 1.2 by default

--- pinax/projects/cms_project_company/settings.py
+++ pinax/projects/cms_project_company/settings.py
@@ -92,6 +92,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django_openid.consumer.SessionConsumer',
     'account.middleware.LocaleMiddleware',
--- pinax/projects/company_project/settings.py
+++ pinax/projects/company_project/settings.py
@@ -87,6 +87,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.doc.XViewMiddleware',
 )
