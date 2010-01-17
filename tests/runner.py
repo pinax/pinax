21e9f8dda2096474d981c4d1e0e888d4066c2f61
Brian Rosner <brosner@gmail.com>
2010-01-16 20:38:25 -0700

    Added MessageMiddleware to MIDDLEWARE_CLASSES in test runner

--- tests/runner.py
+++ tests/runner.py
@@ -94,6 +94,7 @@ def setup_test_environment():
             "django.middleware.common.CommonMiddleware",
             "django.contrib.sessions.middleware.SessionMiddleware",
             "django.contrib.auth.middleware.AuthenticationMiddleware",
+            "django.contrib.messages.middleware.MessageMiddleware",
         ],
         "INSTALLED_APPS": apps,
         "LOGIN_URL": "/account/login/",
