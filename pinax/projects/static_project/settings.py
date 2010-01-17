b317f3da190a511ad69ff1f57ee44d0d7e55901b
Brian Rosner <brosner@gmail.com>
2010-01-16 18:03:57 -0700

    Added local_settings import in static_project settings.py

--- pinax/projects/static_project/settings.py
+++ pinax/projects/static_project/settings.py
@@ -122,3 +122,10 @@ SITE_NAME = ""
 DEBUG_TOOLBAR_CONFIG = {
     "INTERCEPT_REDIRECTS": False,
 }
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from local_settings import *
+except ImportError:
+    pass
