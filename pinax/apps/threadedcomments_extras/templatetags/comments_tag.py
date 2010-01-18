c3594c0b6be7e812c32d60935f01ee33d93bcfe3
Brian Rosner <brosner@gmail.com>
2010-01-17 18:08:35 -0700

    Updated threadedcomments_extras app to new style guidelines

--- pinax/apps/threadedcomments_extras/templatetags/comments_tag.py
+++ pinax/apps/threadedcomments_extras/templatetags/comments_tag.py
@@ -1,12 +1,15 @@
 from django import template
 
+
+
 register = template.Library()
 
+
+
+@register.inclusion_tag("threadedcomments/comments.html", takes_context=True)
 def comments(context, obj):
     return {
-        'object': obj,
-        'request': context['request'],
-        'user': context['user'],
+        "object": obj,
+        "request": context["request"],
+        "user": context["user"],
     }
-
-register.inclusion_tag('threadedcomments/comments.html', takes_context=True)(comments)
