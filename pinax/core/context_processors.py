a5ec18e8f21620cafde6ca86db114293053f744b
Brian Rosner <brosner@gmail.com>
2010-01-17 18:44:36 -0700

    Updated pinax.core to new style guidelines

--- pinax/core/context_processors.py
+++ pinax/core/context_processors.py
@@ -2,6 +2,8 @@ from django.conf import settings
 
 from template_utils.context_processors import settings_processor
 
+
+
 pinax_settings = settings_processor(
-    'CONTACT_EMAIL', 'SITE_NAME', 'STATIC_URL'
+    "CONTACT_EMAIL", "SITE_NAME", "STATIC_URL"
 )
--- pinax/core/management/commands/clone_project.py
+++ pinax/core/management/commands/clone_project.py
@@ -1,22 +1,31 @@
 import glob
 import os
 import optparse
-import sys
-import shutil
-import re
 import random
-import pinax
+import re
+import shutil
+import sys
 
 from optparse import make_option
+
 from django.core.management.base import BaseCommand, CommandError
 
-EXCLUDED_PATTERNS = ('.svn','.pyc',)
+import pinax
+
+
+
+EXCLUDED_PATTERNS = (
+    ".svn",
+    ".pyc",
+)
 DEFAULT_PINAX_ROOT = None # fallback to the normal PINAX_ROOT in settings.py.
-PINAX_ROOT_RE = re.compile(r'PINAX_ROOT\s*=.*$', re.M)
-SECRET_KEY_RE = re.compile(r'SECRET_KEY\s*=.*$', re.M)
-ROOT_URLCONF_RE = re.compile(r'ROOT_URLCONF\s*=.*$', re.M)
-VIRTUALENV_BASE_RE = re.compile(r'VIRTUALENV_BASE\s*=.*$', re.M)
-CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+PINAX_ROOT_RE = re.compile(r"PINAX_ROOT\s*=.*$", re.M)
+SECRET_KEY_RE = re.compile(r"SECRET_KEY\s*=.*$", re.M)
+ROOT_URLCONF_RE = re.compile(r"ROOT_URLCONF\s*=.*$", re.M)
+VIRTUALENV_BASE_RE = re.compile(r"VIRTUALENV_BASE\s*=.*$", re.M)
+CHARS = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
+
+
 
 def get_pinax_root(default_pinax_root):
     if default_pinax_root is None:
@@ -25,20 +34,22 @@ def get_pinax_root(default_pinax_root):
 
 
 def get_projects_dir(pinax_root):
-    return os.path.join(pinax_root, 'projects')
+    return os.path.join(pinax_root, "projects")
 
 
 def get_projects(pinax_root):
     projects = []
-    for item in glob.glob(os.path.join(get_projects_dir(pinax_root), '*')):
+    for item in glob.glob(os.path.join(get_projects_dir(pinax_root), "*")):
         if os.path.isdir(item):
             projects.append(item)
     return projects
+
 
 try:
     WindowsError
 except NameError:
     WindowsError = None
+
 
 def copytree(src, dst, symlinks=False):
     """
@@ -82,31 +93,28 @@ def copytree(src, dst, symlinks=False):
 
 
 def update_settings(pinax_root, path, old_name, new_name):
-    settings_file = open(path, 'r')
+    settings_file = open(path, "r")
     settings = settings_file.read()
     settings_file.close()
     settings = settings.replace(old_name, new_name)
     if pinax_root is not None:
-        settings = PINAX_ROOT_RE.sub("PINAX_ROOT = '%s'" % (pinax_root,),
-            settings)
-    new_secret_key = ''.join([random.choice(CHARS) for i in xrange(50)])
-    settings = SECRET_KEY_RE.sub("SECRET_KEY = '%s'" % (new_secret_key,),
-        settings)
-    new_root_urlconf = '%s.urls' % new_name
-    settings = ROOT_URLCONF_RE.sub("ROOT_URLCONF = '%s'" % new_root_urlconf,
-        settings)
-    settings_file = open(path, 'w')
+        settings = PINAX_ROOT_RE.sub('PINAX_ROOT = "%s"' % pinax_root, settings)
+    new_secret_key = "".join([random.choice(CHARS) for i in xrange(50)])
+    settings = SECRET_KEY_RE.sub('SECRET_KEY = "%s"' % new_secret_key, settings)
+    new_root_urlconf = "%s.urls" % new_name
+    settings = ROOT_URLCONF_RE.sub('ROOT_URLCONF = "%s"' % new_root_urlconf, settings)
+    settings_file = open(path, "w")
     settings_file.write(settings)
     settings_file.close()
 
 
 def update_rename_deploy_files(path, old_name, new_name):
-    for deploy_file in glob.glob(os.path.join(path, "pinax") + '*'):
-        df = open(deploy_file, 'r')
+    for deploy_file in glob.glob(os.path.join(path, "pinax") + "*"):
+        df = open(deploy_file, "r")
         deploy_settings = df.read()
         df.close()
         deploy_settings = deploy_settings.replace(old_name, new_name)
-        df = open(deploy_file, 'w')
+        df = open(deploy_file, "w")
         df.write(deploy_settings)
         df.close()
     # fix modpython.py
@@ -133,7 +141,7 @@ def main(default_pinax_root, project_name, destination, verbose=True):
         raise CommandError("'%s' conflicts with the name of an existing Python module and cannot be used as a project name. Please try another name." % destination)
     
     if os.path.exists(destination):
-        raise CommandError("Files already exist at this path: %s" % (destination,))
+        raise CommandError("Files already exist at this path: %s" % destination)
     user_project_name = os.path.basename(destination)
     pinax_root = get_pinax_root(default_pinax_root)
     if project_name in map(os.path.basename, get_projects(pinax_root)):
@@ -149,11 +157,11 @@ def main(default_pinax_root, project_name, destination, verbose=True):
     copytree(source, destination)
     if verbose:
         print "Updating settings.py for your new project"
-    update_settings(default_pinax_root, os.path.join(destination, 'settings.py'),
+    update_settings(default_pinax_root, os.path.join(destination, "settings.py"),
         project_name, user_project_name)
     if verbose:
         print "Renaming and updating your deployment files"
-    update_rename_deploy_files(os.path.join(destination, 'deploy'), project_name,
+    update_rename_deploy_files(os.path.join(destination, "deploy"), project_name,
         user_project_name)
     if verbose:
         print "Finished cloning your project, now you may enjoy Pinax!"
@@ -164,20 +172,19 @@ class Command(BaseCommand):
     args = "<original_project> <new_project_name>"
     
     clone_project_options = (
-        make_option('-l', '--list-projects', dest='list_projects',
-            action = 'store_true',
-            help = 'lists the projects that are available on this system'),
-        make_option('-r', '--pinax-root', dest='pinax_root',
+        make_option("-l", "--list-projects", dest="list_projects",
+            action = "store_true",
+            help = "lists the projects that are available on this system"),
+        make_option("-r", "--pinax-root", dest="pinax_root",
             default = DEFAULT_PINAX_ROOT,
-            action = 'store_true',
-            help = 'where Pinax lives on your system (defaults to Pinax in your virtual environment)'),
-        make_option('-b', '--verbose', dest='verbose',
-            action = 'store_false', default=True,
-            help = 'enables verbose output'),
+            action = "store_true",
+            help = "where Pinax lives on your system (defaults to Pinax in your virtual environment)"),
+        make_option("-b", "--verbose", dest="verbose",
+            action = "store_false", default=True,
+            help = "enables verbose output"),
     )
     
     option_list = BaseCommand.option_list + clone_project_options
-    
     
     def handle(self, *args, **options):
         """
@@ -185,16 +192,16 @@ class Command(BaseCommand):
         operations.
         """
         
-        if options.get('list_projects'):
-            pinax_root = get_pinax_root(options.get('pinax_root'))
+        if options.get("list_projects"):
+            pinax_root = get_pinax_root(options.get("pinax_root"))
             print "Available Projects"
             print "------------------"
             sys.path.insert(0, get_projects_dir(pinax_root))
             for project in map(os.path.basename, get_projects(pinax_root)):
                 print "%s:" % project
-                about = getattr(__import__(project), '__about__', '')
+                about = getattr(__import__(project), "__about__", "")
                 for line in about.strip().splitlines():
-                    print '    %s' % line
+                    print "    %s" % line
                 print
             sys.exit(0)
         
@@ -211,7 +218,7 @@ class Command(BaseCommand):
             self.print_help("pinax-admin", "clone_project")
             sys.exit(0)
         
-        main(options.get('pinax_root'), args[0], destination,
-            verbose = options.get('verbose')
+        main(options.get("pinax_root"), args[0], destination,
+            verbose = options.get("verbose")
         )
         return 0
