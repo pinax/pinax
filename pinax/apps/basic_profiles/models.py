68da8366f6b085f738abd3913bd4039c37b62972
Daniel Greenfeld <pydanny@gmail.com>
2010-02-20 22:01:48 -0500

    Adding reverse to models.py of basic profiles

--- pinax/apps/basic_profiles/models.py
+++ pinax/apps/basic_profiles/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
