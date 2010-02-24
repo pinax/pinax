757309c32f2e5b3514fa8cf094316930e2960694
James Tauber <jtauber@jtauber.com>
2010-02-24 15:47:23 -0500

    fixed whitespace that violated coding conventions

--- pinax/apps/signup_codes/tests/test_invite_user.py
+++ pinax/apps/signup_codes/tests/test_invite_user.py
@@ -24,29 +24,24 @@ class InviteUserTest(TestCase):
         # remove django-mailer to properly test for outbound e-mail
         if "mailer" in settings.INSTALLED_APPS:
             settings.INSTALLED_APPS.remove("mailer")
-            
+        
         self.old_template_dirs = settings.TEMPLATE_DIRS
         
         settings.TEMPLATE_DIRS = [
-
             os.path.join(os.path.dirname(signup_codes.__file__), "tests", "templates"),
-
         ]
-
-            
     
     def tearDown(self):
-        settings.INSTALLED_APPS = self.old_installed_apps    
+        settings.INSTALLED_APPS = self.old_installed_apps
         settings.TEMPLATE_DIRS = self.old_template_dirs
         
     def _create_user(self, username="tester", password="abc1234", email="test@example.com"):
         admin_user = User.objects.create_user(username, email, password)
-        admin_user.save()        
+        admin_user.save()
         
     def test_noauth_admin_invite_user(self):
         """
-        
-        Confirm that a user cannot submit an invitation without being 
+        Confirm that a user cannot submit an invitation without being
         authenticated as an admin user.
         """
         
@@ -59,14 +54,13 @@ class InviteUserTest(TestCase):
         # Then they attempt as a logged in user without admin rights.
         user = User.objects.create_user("joe_tester","joe@example.com", "tester")
         user.save()
-        self.client.login(username="joe_tester", password="tester")         
+        self.client.login(username="joe_tester", password="tester")
         response = self.client.post(reverse("admin_invite_user"), data)
-        self.assertEqual(response.status_code, 302)        
-        
+        self.assertEqual(response.status_code, 302)
     
     def test_admin_invite_user(self):
         """
-            Test on the admin invite user screen
+        Test on the admin invite user screen
         """
         
         # Create and authenticate a staff level user
@@ -75,21 +69,20 @@ class InviteUserTest(TestCase):
         admin_user.is_staff = True
         admin_user.save()
         
-        self.client.login(username="tester", password="tester")        
-
+        self.client.login(username="tester", password="tester")
+        
         # Now confirm that the invite is sent out by the staff level user
         data = {"email":"bob@example.com"}
         response = self.client.post(reverse("admin_invite_user"), data)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "An e-mail has been sent to " + data["email"])
-
-        
+    
     def test_accept_invite(self):
         """
-            test the user's ability to accept their token
+        test the user's ability to accept their token
         """
-
-        #print help(self)        
+        
+        #print help(self)
         # Create an invitation
         email = "joe@example.com"
         expiry = datetime.now() + timedelta(hours=1)
@@ -111,12 +104,11 @@ class InviteUserTest(TestCase):
         # Now they remember the code and try the right one
         data = { "code" : signup_code.code }
         response = self.client.get(reverse("test_signup"), data, follow=True)
-
         
         self.assertContains(response, "id_username")
         self.assertContains(response, "id_email")
-        self.assertContains(response, "id_password")                
-
+        self.assertContains(response, "id_password")
+        
         # User now creates their account
         data = { "username" : "joe",
                  "email": "joe@example.com",
@@ -127,6 +119,3 @@ class InviteUserTest(TestCase):
                 }
         response = self.client.post(reverse("test_signup"), data, follow=True)
         print User.objects.all()
-        
-        
-        
