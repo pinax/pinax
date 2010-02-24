8a6e23a78022752a65187724ebf8e0cfe9841615
Brian Rosner <brosner@gmail.com>
2010-02-24 03:04:16 -0500

    Fixed #424 — added attrs support to {% silk %}
    Patch from Kenneth Belitzky and cleaned up.

--- pinax/templatetags/templatetags/theme_tags.py
+++ pinax/templatetags/templatetags/theme_tags.py
@@ -7,9 +7,45 @@ register = template.Library()
 
 
 
-@register.simple_tag
-def silk(name):
-    return """<img src="%spinax/images/silk/icons/%s.png" />""" % (
-        settings.STATIC_URL,
-        name
-    )
+class SilkNode(template.Node):
+    """
+    node class for silk tag
+    """
+    
+    def __init__(self, name, attrs):
+        self.name = template.Variable(name)
+        self.attrs = {}
+        for attr in attrs:
+            key, value = attr.split("=")
+            self.attrs[key] = template.Variable(value)
+    
+    def render(self, context):
+        """
+        render the img tag with specified attributes
+        """
+        name = self.name.resolve(context)
+        attrs = []
+        for k, v in self.attrs.iteritems():
+            attrs.append('%s="%s"' % (k, v.resolve(context)))
+        if attrs:
+            attrs = " %s" % " ".join(attrs)
+        else:
+            attrs = ""
+        return """<img src="%spinax/images/silk/icons/%s.png"%s/>""" % (
+            settings.STATIC_URL,
+            name,
+            attrs,
+        )
+
+
+@register.tag
+def silk(parser, token):
+    """
+    Template tag to render silk icons
+    Usage::
+    
+        {{ silk "image_name" arg1="value1" arg2="value2" ... }}
+    
+    """
+    bits = token.split_contents()
+    return SilkNode(bits[1], bits[2:])
