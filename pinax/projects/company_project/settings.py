540a5acfd410e5cc4f8426c27bb81b233f56b010
Brian Rosner <brosner@gmail.com>
2010-02-28 14:07:30 -0700

    Marked BIBLION_SECTIONS strings for translation (moved the location of setting too)

--- pinax/projects/company_project/settings.py
+++ pinax/projects/company_project/settings.py
@@ -4,12 +4,10 @@ import os.path
 import posixpath
 import pinax
 
+ugettext = lambda s: s
+
 PINAX_ROOT = os.path.abspath(os.path.dirname(pinax.__file__))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-BIBLION_SECTIONS = [
-    ("technical", "Technical"),
-    ("business", "Business"),
-]
 
 # tells Pinax to use the default theme
 PINAX_THEME = "default"
@@ -145,6 +143,11 @@ INSTALLED_APPS = [
 ]
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+BIBLION_SECTIONS = [
+    ("technical", ugetext(u"Technical")),
+    ("business", ugetext(u"Business")),
+]
 
 #TWITTER_USERNAME = ""
 #TWITTER_PASSWORD = ""
