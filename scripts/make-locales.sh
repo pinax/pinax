773655df07cc6b46609a20fbbcef46aa41840e58
Jannis Leidel <jannis@leidel.info>
2010-03-02 19:11:45 +0100

    Added all locales supported by Django to the make-locales.sh script. Also require a subcommand "make" or "compile".

--- scripts/make-locales.sh
+++ scripts/make-locales.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 here=`pwd`
 cd ..
+
+CMD="$1"
+
 for dir in pinax pinax/apps/* pinax/projects/* pinax/projects/*/apps/*
 do
   if [ -d "${dir}" ]
@@ -8,12 +11,20 @@ do
     cd ${dir}
     echo "Making/updating locales of '${dir}'"
     mkdir -p locale
-    # hardcode locales for now (with the old list from 0.5.X days)
-    for locale in en de es fr sv pt_BR he ar it
-    do
-      pinax-admin makemessages -l ${locale} -e html,txt -d django
-    done
-    django-admin.py compilemessages
+    # hardcode locales for now
+    case $CMD in
+      "make")
+        for locale in ar bg bn bs ca cs cy da de el en es es_AR et eu fa fi fr fy_NL ga gl he hi hr hu is it ja ka km kn ko lt lv mk nl no pl pt pt_BR ro ru sk sl sq sr sr_Latn sv ta te th tr uk vi zh_CN zh_TW
+        do
+          pinax-admin makemessages -l ${locale} -e html,txt -d django
+        done
+        ;;
+      "compile")
+        django-admin.py compilemessages
+        ;;
+      *)
+        ;;
+    esac
     cd ${here}/..
   fi
 done
