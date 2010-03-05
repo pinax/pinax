d40de563bfcbf4f4c8490bd94bf9c5f6f6e05ef1
Jannis Leidel <jannis@leidel.info>
2010-03-05 22:16:29 +0100

    Added find_package_data to setup.py to find package_data instead of using setuptools-dummy. Also added a manifest template for the source distribution, required to be kept up-to-date.

--- setup.py
+++ setup.py
@@ -1,10 +1,98 @@
 import os
+import sys
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
 from setuptools import setup, find_packages
 
 VERSION = __import__('pinax').__version__
 
 def read(*path):
     return open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *path)).read()
+
+# Provided as an attribute, so you can append to these instead
+# of replicating them:
+standard_exclude = ('*.py', '*.pyc', '*~', '.*', '*.bak')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build',
+                                './dist', 'EGG-INFO', '*.egg-info')
+
+# Copied from paste/util/finddata.py
+def find_package_data(where='.', package='', exclude=standard_exclude,
+        exclude_directories=standard_exclude_directories,
+        only_in_packages=True, show_ignored=False):
+    """
+    Return a dictionary suitable for use in ``package_data``
+    in a distutils ``setup.py`` file.
+
+    The dictionary looks like::
+
+        {'package': [files]}
+
+    Where ``files`` is a list of all the files in that package that
+    don't match anything in ``exclude``.
+
+    If ``only_in_packages`` is true, then top-level directories that
+    are not packages won't be included (but directories under packages
+    will).
+
+    Directories matching any pattern in ``exclude_directories`` will
+    be ignored; by default directories with leading ``.``, ``CVS``,
+    and ``_darcs`` will be ignored.
+
+    If ``show_ignored`` is true, then all the files that aren't
+    included in package data are shown on stderr (for debugging
+    purposes).
+
+    Note patterns use wildcards, or can be exact paths (including
+    leading ``./``), and all searching is case-insensitive.
+    """
+
+    out = {}
+    stack = [(convert_path(where), '', package, only_in_packages)]
+    while stack:
+        where, prefix, package, only_in_packages = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print >> sys.stderr, (
+                                "Directory %s ignored by pattern %s"
+                                % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                if (os.path.isfile(os.path.join(fn, '__init__.py'))
+                    and not prefix):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                    stack.append((fn, '', new_package, False))
+                else:
+                    stack.append((fn, prefix + name + '/', package, only_in_packages))
+            elif package or not only_in_packages:
+                # is a file
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print >> sys.stderr, (
+                                "File %s ignored by pattern %s"
+                                % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+excluded_directories = standard_exclude_directories + ('./requirements', './scripts')
+package_data = find_package_data(exclude_directories=excluded_directories)
 
 setup(
     name='Pinax',
@@ -17,15 +105,8 @@ setup(
     maintainer_email='jannis@leidel.info',
     url='http://pinaxproject.com/',
     packages=find_packages(),
-    include_package_data=True,
-    # Ignore the tarballs we built our own in a source distribution
-    exclude_package_data={
-        'requirements': ['%s/*.tar.gz' % VERSION],
-    },
+    package_data=package_data,
     zip_safe=False,
-    setup_requires=[
-        'setuptools_dummy',
-    ],
     entry_points={
         'console_scripts': [
             'pinax-admin = pinax.core.management:execute_from_command_line',
