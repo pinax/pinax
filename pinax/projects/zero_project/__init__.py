8c3eaaa1ba2ac373cc052e580f508113d8ec7003
James Tauber <jtauber@jtauber.com>
2010-03-06 11:16:03 +0000

    rewrote zero_project.__about__ description

--- pinax/projects/zero_project/__init__.py
+++ pinax/projects/zero_project/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __about__ = """
-This project comes with the bare minimum set of applications and templates
-to get you started. It includes no extra tabs, only the profile and notices
-tabs are included by default. From here you can add any extra functionality
-and applications that you would like.
+This project lays the foundation for all other Pinax starter projects. It
+provides the project directory layout and some key infrastructure apps on
+which the other starter projects are based.
 """
