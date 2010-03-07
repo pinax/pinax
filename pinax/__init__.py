fae501041857f1e4eea2b5157feb94a3f3c84f18
Brian Rosner <brosner@gmail.com>
2010-03-06 23:08:44 -0700

    Support development versions using an environment variable

--- pinax/__init__.py
+++ pinax/__init__.py
@@ -1,3 +1,6 @@
+import os
+
+
 VERSION = (0, 9, 0, "a", 1) # following PEP 386
 
 
@@ -7,6 +10,9 @@ def get_version():
         version = "%s.%s" % (version, VERSION[2])
     if VERSION[3] != "f":
         version = "%s%s%s" % (version, VERSION[3], VERSION[4])
+        dev = os.environ.get("PINAX_DEV_N")
+        if dev:
+            version = "%s.dev%s" % (version, dev)
     return version
 
 
