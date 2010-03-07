f3a4bdc3dd54d3a051c8c86e48eeee1cebd8c688
Brian Rosner <brosner@gmail.com>
2010-03-06 22:40:57 -0700

    Minor clean-up in pinax/__init__.py

--- pinax/__init__.py
+++ pinax/__init__.py
@@ -1,11 +1,13 @@
 VERSION = (0, 9, 0, "alpha", 1)
 
+
 def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
+    version = "%s.%s" % (VERSION[0], VERSION[1])
     if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
+        version = "%s.%s" % (version, VERSION[2])
     if VERSION[3] != "final":
-        version = '%s%s%s' % (version, VERSION[3], VERSION[4])
+        version = "%s%s%s" % (version, VERSION[3], VERSION[4])
     return version
+
 
 __version__ = get_version()
