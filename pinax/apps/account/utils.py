340296e5b3d7cb14683208438422d096d48a17bc
Brian Rosner <brosner@gmail.com>
2010-05-05 22:54:59 -0600

    Try looking in the session for a redirect value in get_default_redirect

--- pinax/apps/account/utils.py
+++ pinax/apps/account/utils.py
@@ -8,7 +8,7 @@ LOGIN_REDIRECT_URLNAME = getattr(settings, "LOGIN_REDIRECT_URLNAME", "")
 
 
 def get_default_redirect(request, redirect_field_name="next",
-        login_redirect_urlname=LOGIN_REDIRECT_URLNAME):
+        login_redirect_urlname=LOGIN_REDIRECT_URLNAME, session_key_value="redirect_to"):
     """
     Returns the URL to be used in login procedures by looking at different
     values in the following order:
@@ -22,6 +22,10 @@ def get_default_redirect(request, redirect_field_name="next",
     else:
         default_redirect_to = settings.LOGIN_REDIRECT_URL
     redirect_to = request.REQUEST.get(redirect_field_name)
+    if not redirect_to:
+        # try the session if available
+        if hasattr(request, "session"):
+            redirect_to = request.session.get(session_key_value)
     # light security check -- make sure redirect_to isn't garabage.
     if not redirect_to or "://" in redirect_to or " " in redirect_to:
         redirect_to = default_redirect_to
