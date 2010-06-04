ea3e7a011e7ec26e130ceeb3e9e5b774c1ea5f59
Patrick Altman <paltman@gmail.com>
2010-06-03 22:44:09 -0500

    getting started on documenting apps for pinax

--- docs/conf.py
+++ docs/conf.py
@@ -15,14 +15,17 @@ import sys, os
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-#sys.path.append(os.path.abspath('some/directory'))
+sys.path.append(os.path.abspath('../pinax'))
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 
 # General configuration
 # ---------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
