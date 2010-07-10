157ca59dd0ae3e0c9c694bba83aef8b037adced6
Brian Rosner <brosner@gmail.com>
2010-07-10 14:08:03 -0600

    Updated account_project with some missing URLpatterns

--- pinax/projects/account_project/urls.py
+++ pinax/projects/account_project/urls.py
@@ -5,6 +5,8 @@ from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
+from pinax.apps.account.openid_consumer import PinaxConsumer
+
 
 handler500 = "pinax.views.server_error"
 
@@ -13,8 +15,11 @@ urlpatterns = patterns("",
     url(r"^$", direct_to_template, {
         "template": "homepage.html",
     }, name="home"),
+    url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
     url(r"^admin/", include(admin.site.urls)),
+    url(r"^about/", include("about.urls")),
     url(r"^account/", include("pinax.apps.account.urls")),
+    url(r"^openid/(.*)", PinaxConsumer()),
 )
 
 
