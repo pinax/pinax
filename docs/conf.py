caff4a3d61866918b57d4bdc3e371685281d5126
Brian Rosner <brosner@gmail.com>
2010-09-11 13:37:29 -0700

    Updated Sphinx conf.py release variable for 0.9a2

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ copyright = '2010, James Tauber and Pinax Team'
 # The short X.Y version.
 version = '0.9'
 # The full version, including alpha/beta/rc tags.
-release = '0.9a1'
+release = '0.9a2'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
