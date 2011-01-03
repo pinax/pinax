9a4c15aad33d0e26254933f2d4c750f98a4ea395
Brian Rosner <brosner@gmail.com>
2011-01-02 17:31:22 -0700

    Do not use the deprecated django.core.context_processors.auth context processor

--- tests/runner.py
+++ tests/runner.py
@@ -101,7 +101,7 @@ def setup_test_environment():
         # these settings are currently required to support Pinax default
         # templates.
         "TEMPLATE_CONTEXT_PROCESSORS": [
-            "django.core.context_processors.auth",
+            "django.contrib.auth.context_processors.auth",
             "django.core.context_processors.debug",
             "django.core.context_processors.i18n",
             "django.core.context_processors.media",
