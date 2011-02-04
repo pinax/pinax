77d303a6615d03d7e41c03482c42fd8fe92b2c26
Brian Rosner <brosner@gmail.com>
2011-02-03 22:48:30 -0700

    Moved over setup.py and MANIFEST.in
    Decided to leave them as-is even if some patterns do not match in MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,136 @@
+import os
+import sys
+
+from fnmatch import fnmatchcase
+
+from distutils.util import convert_path
+from setuptools import setup, find_packages
+
+
+VERSION = __import__("pinax").__version__
+
+
+def read(*path):
+    return open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *path)).read()
+
+
+# Provided as an attribute, so you can append to these instead
+# of replicating them:
+standard_exclude = ["*.py", "*.pyc", "*~", ".*", "*.bak"]
+standard_exclude_directories = [
+    ".*", "CVS", "_darcs", "./build",
+    "./dist", "EGG-INFO", "*.egg-info"
+]
+
+# Copied from paste/util/finddata.py
+def find_package_data(where=".", package="", exclude=standard_exclude,
+        exclude_directories=standard_exclude_directories,
+        only_in_packages=True, show_ignored=False):
+    """
+    Return a dictionary suitable for use in ``package_data``
+    in a distutils ``setup.py`` file.
+    
+    The dictionary looks like::
+    
+        {"package": [files]}
+    
+    Where ``files`` is a list of all the files in that package that
+    don't match anything in ``exclude``.
+    
+    If ``only_in_packages`` is true, then top-level directories that
+    are not packages won't be included (but directories under packages
+    will).
+    
+    Directories matching any pattern in ``exclude_directories`` will
+    be ignored; by default directories with leading ``.``, ``CVS``,
+    and ``_darcs`` will be ignored.
+    
+    If ``show_ignored`` is true, then all the files that aren't
+    included in package data are shown on stderr (for debugging
+    purposes).
+    
+    Note patterns use wildcards, or can be exact paths (including
+    leading ``./``), and all searching is case-insensitive.
+    """
+    
+    out = {}
+    stack = [(convert_path(where), "", package, only_in_packages)]
+    while stack:
+        where, prefix, package, only_in_packages = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print >> sys.stderr, (
+                                "Directory %s ignored by pattern %s"
+                                % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                if (os.path.isfile(os.path.join(fn, "__init__.py"))
+                    and not prefix):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + "." + name
+                    stack.append((fn, "", new_package, False))
+                else:
+                    stack.append((fn, prefix + name + "/", package, only_in_packages))
+            elif package or not only_in_packages:
+                # is a file
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print >> sys.stderr, (
+                                "File %s ignored by pattern %s"
+                                % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+
+excluded_directories = standard_exclude_directories + ["./requirements", "./scripts"]
+package_data = find_package_data(exclude_directories=excluded_directories)
+
+
+setup(
+    name="Pinax",
+    version=VERSION,
+    description="Pinax is an open-source collection of re-usable apps for the Django Web Framework",
+    long_description=read("docs", "intro.txt"),
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    maintainer="Brian Rosner",
+    maintainer_email="brosner@gmail.com",
+    url="http://pinaxproject.com/",
+    download_url="http://pinaxproject.com/downloads/",
+    packages=find_packages(),
+    package_data=package_data,
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "pinax-admin = pinax.core.management:execute_from_command_line",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.4",
+        "Programming Language :: JavaScript",
+    ],
+)
