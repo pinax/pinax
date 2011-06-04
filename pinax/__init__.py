pinax/pinax

commit 1e857010a5cb5bba8d428d68a7ee6b8957545dfd

Bumped version

pinax/__init__.py
@@ -1,5 +1,5 @@
 VERSION = (0, 9, 0, "a", 2) # following PEP 386
-DEV_N = 1011
+DEV_N = 1012
 
 
 def get_version():

commit 28a53e046cd760a77616f535debe30f80c3284ec

Fixed bug where if deployment file happens to be named the same as we expect in our deploy folder it will set incorrect relative_project_path (broken in 1e85701)

pinax/__init__.py
@@ -1,5 +1,5 @@
 VERSION = (0, 9, 0, "a", 2) # following PEP 386
-DEV_N = 1012
+DEV_N = 1013
 
 
 def get_version():

pinax/env.py
@@ -14,7 +14,7 @@ def setup_environ(dunder_file=None, project_path=None, relative_project_path=Non
             "fcgi.py", "fcgi.pyc",
             "wsgi.py", "wsgi.pyc",
         ]
-        if os.path.basename(dunder_file) in deploy_files:
+        if os.path.basename(dunder_file) in deploy_files and relative_project_path is None:
             relative_project_path = [os.pardir]
         if relative_project_path is not None:
             project_path = os.path.abspath(os.path.join(file_path, *relative_project_path))