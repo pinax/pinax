4f139bac7bf90719712dbde7c30d319cfe49870e
Donald Stufft <donald.stufft@gmail.com>
2011-08-03 16:57:32 -0400

    add searching SignupCode for code and email in the admin

--- pinax/apps/signup_codes/admin.py
+++ pinax/apps/signup_codes/admin.py
@@ -6,6 +6,7 @@ from pinax.apps.signup_codes.models import SignupCode
 
 class SignupCodeAdmin(admin.ModelAdmin):
     list_display = ["code", "max_uses", "use_count", "expiry", "created"]
+    search_fields = ["code", "email"]
     list_filter = ["created"]
 
 
