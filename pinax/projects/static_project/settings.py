eb8fd64b4c0cd5592befd5a7961b25cf844aae47
James Tauber <jtauber@jtauber.com>
2011-09-06 17:01:05 -0700

    removed unnecessary import and setting

--- pinax/projects/static_project/settings.py
+++ pinax/projects/static_project/settings.py
@@ -3,9 +3,7 @@
 
 import os.path
 import posixpath
-import pinax
 
-PINAX_ROOT = os.path.abspath(os.path.dirname(pinax.__file__))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
