d08d79fa2c93ec6ad3e351f541f3c21f55e13d4f
Patrick Altman <paltman@gmail.com>
2011-10-23 21:41:54 -0500

    Add signup codes expired stat

--- pinax/apps/signup_codes/stats.py
+++ pinax/apps/signup_codes/stats.py
@@ -1,3 +1,5 @@
+import datetime
+
 from pinax.apps.signup_codes.models import SignupCode
 
 
@@ -5,5 +7,6 @@ def stats():
     return {
         "signup_codes_total": SignupCode.objects.count(),
         "signup_codes_sent": SignupCode.objects.filter(sent__isnull=True).count(),
-        "signup_codes_used": SignupCode.objects.filter(use_count__gt=0).count()
+        "signup_codes_used": SignupCode.objects.filter(use_count__gt=0).count(),
+        "signup_codes_expired": SignupCode.objects.exclude(expiry__isnull=True).filter(expiry__lte=datetime.datetime.now(), use_count=0).
     }
