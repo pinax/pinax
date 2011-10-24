79ace31c6f5b2eda45ff0a29ecc36031a2b0de41
Patrick Altman <paltman@gmail.com>
2011-10-23 21:43:46 -0500

    Dumb mistake from working to fast

--- pinax/apps/signup_codes/stats.py
+++ pinax/apps/signup_codes/stats.py
@@ -8,5 +8,5 @@ def stats():
         "signup_codes_total": SignupCode.objects.count(),
         "signup_codes_sent": SignupCode.objects.filter(sent__isnull=True).count(),
         "signup_codes_used": SignupCode.objects.filter(use_count__gt=0).count(),
-        "signup_codes_expired": SignupCode.objects.exclude(expiry__isnull=True).filter(expiry__lte=datetime.datetime.now(), use_count=0).
+        "signup_codes_expired": SignupCode.objects.exclude(expiry__isnull=True).filter(expiry__lte=datetime.datetime.now(), use_count=0)
     }
