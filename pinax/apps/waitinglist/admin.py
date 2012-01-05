d28cfa9b0dc2b7448836e862dbb720d4d689ae1f
Patrick Altman <paltman@gmail.com>
2012-01-05 16:02:09 -0600

    Add email as a search field in admin

--- pinax/apps/waitinglist/admin.py
+++ pinax/apps/waitinglist/admin.py
@@ -5,6 +5,7 @@ from pinax.apps.waitinglist.models import WaitingListEntry
 
 class WaitingListEntryAdmin(admin.ModelAdmin):
     list_display = ["email", "created"]
+    search_fields = ["email"]
 
 
 admin.site.register(WaitingListEntry, WaitingListEntryAdmin)
