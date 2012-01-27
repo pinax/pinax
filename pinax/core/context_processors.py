044ea16646bf824de1c339dac74c4911416bf434
Brian Rosner <brosner@gmail.com>
2012-01-27 16:00:39 -0700

    Fixed pinax.core.context_processors.pinax_settings to work without sites app

--- pinax/core/context_processors.py
+++ pinax/core/context_processors.py
@@ -4,8 +4,13 @@ from django.contrib.sites.models import Site
 
 
 def pinax_settings(request):
-    site = Site.objects.get_current()
-    return {
-        "SITE_NAME": site.name,
-        "SITE_DOMAIN": site.domain
-    }
+    ctx = {}
+    
+    if Site._meta.installed:
+        site = Site.objects.get_current()
+        ctx.update({
+            "SITE_NAME": site.name,
+            "SITE_DOMAIN": site.domain
+        })
+    
+    return ctx
